37ba99b6ed4e3f12f782781977291b4f399d9f3a
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-05-02 14:08:25 -0600

    Aplicacion de Notificaciones Dummies

--- projects/sivimss-gui/src/app/components/sub-header-privado/sub-header-privado.component.ts
+++ projects/sivimss-gui/src/app/components/sub-header-privado/sub-header-privado.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { UsuarioEnSesion } from "projects/sivimss-gui/src/app/models/usuario-en-sesion.interface";
-import { AutenticacionService } from "projects/sivimss-gui/src/app/services/autenticacion.service";
-import { Subscription } from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UsuarioEnSesion} from "projects/sivimss-gui/src/app/models/usuario-en-sesion.interface";
+import {AutenticacionService} from "projects/sivimss-gui/src/app/services/autenticacion.service";
+import {Subscription} from "rxjs";
+import {NotificacionesService} from "../../services/notificaciones.service";
 
 @Component({
   selector: 'app-sub-header-privado',
   templateUrl: './sub-header-privado.component.html',
-  styleUrls: ['./sub-header-privado.component.scss']
+  styleUrls: ['./sub-header-privado.component.scss'],
+  providers: [NotificacionesService,]
 })
 export class SubHeaderPrivadoComponent implements OnInit, OnDestroy {
+
   usuarioEnSesion!: UsuarioEnSesion | null;
   subs!: Subscription;
+  existeNotificacion: boolean;
+  notificaciones: string[] = [];
 
-  constructor(private readonly autenticacionService: AutenticacionService) {
+  constructor(private readonly autenticacionService: AutenticacionService,
+              private readonly notificacionService: NotificacionesService) {
+    this.existeNotificacion = notificacionService.existenNotificaciones();
+    this.notificaciones = notificacionService.consultarNotificaciones();
   }
 
   ngOnInit(): void {
