7fea4059e279a7e7f213866eeb4fc5906d867466
mikanyel <mikanyel@gmail.com>
2023-05-02 20:51:18 -0600

    Se integra maqueta del CU101 solicitar Vale Paritaria, queda en espera de terminar confirme al figma

---
+++ projects/sivimss-gui/src/app/modules/vales-paritaria/services/vale-paritaria.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { HttpRespuesta } from "../../../models/http-respuesta.interface";
+import { BaseService } from "../../../utils/base-service";
+import { environment } from '../../../../environments/environment';
+
+@Injectable()
+export class ValeParitariaService extends BaseService<HttpRespuesta<any>, any> {
+//TO DO Cambiar el Id de la funcionalidad cuando se obtenga del oaut
+//      Cambiar auth_token2 por el token de la sesion del usuario
+
+  private auth_token3: string = "eyJzaXN0ZW1hIjoic2l2aW1zcyIsImFsZyI6IkhTMjU2In0.eyJzdWIiOiJ7XCJpZFZlbGF0b3Jpb1wiOlwiMVwiLFwiaWRSb2xcIjpcIjFcIixcImRlc1JvbFwiOlwiQ09PUkRJTkFET1IgREUgQ0VOVFJcIixcImlkRGVsZWdhY2lvblwiOlwiMVwiLFwiaWRPZmljaW5hXCI6XCIxXCIsXCJpZFVzdWFyaW9cIjpcIjFcIixcImN2ZVVzdWFyaW9cIjpcIjFcIixcImN2ZU1hdHJpY3VsYVwiOlwiMVwiLFwibm9tYnJlXCI6XCIxIDEgMVwiLFwiY3VycFwiOlwiMVwifSIsImlhdCI6MTY4MDI5MjI4NCwiZXhwIjoxNjgwODk3MDg0fQ.IiVuYNHJLgizTOaGB_cbdO8GKMb7mvTQ0yvsja97-D8";
+   
+  constructor(protected _http: HttpClient) {
+    super(_http, `${environment.api.mssivimss}`, "agregar-rol", "actualizar-rol", 
+      4, "consultar-roles", "detalle-rol", "cambiar-estatus-rol" );
+  }
+
+}
