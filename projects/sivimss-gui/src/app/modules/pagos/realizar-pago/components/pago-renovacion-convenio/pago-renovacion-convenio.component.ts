3850ec05674bee8f2a90897c26b43ca894ad2f55
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-05-08 12:31:33 -0600

    Pago Renovación Convenio Lista

--- projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/pago-renovacion-convenio/pago-renovacion-convenio.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/pago-renovacion-convenio/pago-renovacion-convenio.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {OverlayPanel} from "primeng/overlaypanel";
+import {DIEZ_ELEMENTOS_POR_PAGINA} from "../../../../../utils/constantes";
+import {REGISTROS_PAGOS_ODS} from "../../constants/dummies";
+import {LazyLoadEvent} from "primeng/api";
 
 @Component({
   selector: 'app-pago-renovacion-convenio',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagoRenovacionConvenioComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(OverlayPanel)
+  overlayPanel!: OverlayPanel;
+
+  numPaginaActual: number = 0;
+  cantElementosPorPagina: number = DIEZ_ELEMENTOS_POR_PAGINA;
+  totalElementos: number = 0;
+
+  pagos: any[] = REGISTROS_PAGOS_ODS;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  seleccionarPaginacion(event?: LazyLoadEvent): void {
+    if (event) {
+      this.numPaginaActual = Math.floor((event.first || 0) / (event.rows || 1));
+    }
+  }
+
+  abrirPanel(event: MouseEvent, pago: any) {
+    this.overlayPanel.toggle(event);
   }
 
 }
