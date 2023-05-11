8a7fdbb50301ba2bb2acebdc573cc51f6dacaa45
GRUPOPM\mparedes <martin.paredes@people-media.com.mx>
2023-05-11 14:32:38 -0600

    Ajustes por comentarios de prueba CU 58 - Vales de Salida

--- projects/sivimss-gui/src/app/modules/velacion-domicilio/velacion-domicilio-routing.module.ts
+++ projects/sivimss-gui/src/app/modules/velacion-domicilio/velacion-domicilio-routing.module.ts
@@ -6,6 +6,7 @@ import {
 } from "./components/detalle-velacion-domicilio/detalle-velacion-domicilio.component";
 import { GenerarValeSalidaComponent } from "./components/generar-vale-salida/generar-vale-salida.component";
 import { VelacionDomicilioResolver } from "./services/velacion-domicilio.resolver";
+import { VelacionDomicilioDetalleResolver } from "./services/velacion-domicilio-detalle.resolver";
 
 const routes: Routes = [
   {
@@ -23,10 +24,10 @@ const routes: Routes = [
     }
   },
   {
-    path: 'ver-detalle/:id',
+    path: 'ver-detalle/:idValeSalida',
     component: DetalleVelacionDomicilioComponent,
     resolve: {
-      respuesta: VelacionDomicilioResolver
+      respuesta: VelacionDomicilioDetalleResolver
     }
   }
 ]
@@ -36,6 +37,7 @@ const routes: Routes = [
   exports: [RouterModule],
   providers: [
     VelacionDomicilioResolver,
+    VelacionDomicilioDetalleResolver,
   ]
 })
 
