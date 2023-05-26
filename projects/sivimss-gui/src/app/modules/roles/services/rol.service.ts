44cdebb79b53a12f87fafcd86d46e47e6e1661bb
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-05-26 07:43:23 -0600

    Fix Filtros (revisar)

--- projects/sivimss-gui/src/app/modules/roles/services/rol.service.ts
+++ projects/sivimss-gui/src/app/modules/roles/services/rol.service.ts
@@ -1,4 +1,4 @@
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Observable, of} from "rxjs";
 import {HttpRespuesta} from "../../../models/http-respuesta.interface";
@@ -11,16 +11,25 @@ import {AutenticacionService} from "../../../services/autenticacion.service";
 @Injectable()
 export class RolService extends BaseService<HttpRespuesta<any>, any> {
 
+  private auth_token3: string = "eyJzaXN0ZW1hIjoic2l2aW1zcyIsImFsZyI6IkhTMjU2In0.eyJzdWIiOiJ7XCJpZFZlbGF0b3Jpb1wiOlwiMVwiLFwiaWRSb2xcIjpcIjFcIixcImRlc1JvbFwiOlwiQ09PUkRJTkFET1IgREUgQ0VOVFJcIixcImlkRGVsZWdhY2lvblwiOlwiMVwiLFwiaWRPZmljaW5hXCI6XCIxXCIsXCJpZFVzdWFyaW9cIjpcIjFcIixcImN2ZVVzdWFyaW9cIjpcIjFcIixcImN2ZU1hdHJpY3VsYVwiOlwiMVwiLFwibm9tYnJlXCI6XCIxIDEgMVwiLFwiY3VycFwiOlwiMVwifSIsImlhdCI6MTY4MTE2NTMyNCwiZXhwIjoxNjgxNzcwMTI0fQ.krsXJqvtKlgKlxTvWt2P0cLlGhZDGb9G7vWcNKnD0MU";
+
   constructor(_http: HttpClient, private authService: AutenticacionService) {
     super(_http, `${environment.api.mssivimss}`, "agregar-rol", "actualiza-rol",
       4, "consulta-roles", "detalle-rol", "cambia-estatus");
   }
 
   buscarPorFiltros(filtros: any, pagina: number, tamanio: number): Observable<HttpRespuesta<any>> {
+    const headers: HttpHeaders = new HttpHeaders({
+      Authorization: `Bearer ${this.auth_token3}`,
+      'Content-Type': 'application/json'
+    });
     const params: HttpParams = new HttpParams()
       .append("pagina", pagina)
       .append("tamanio", tamanio);
-    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/buscar/consultar-filtro`, filtros, {params});
+    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/buscar/consultar-filtro`, filtros, {
+      headers,
+      params
+    });
   }
 
   obtenerCatRolesPaginadoSinFiltro(pagina: number, tamanio: number): Observable<HttpRespuesta<any>> {
@@ -40,12 +49,6 @@ export class RolService extends BaseService<HttpRespuesta<any>, any> {
   obtenerCatNivel(): Observable<TipoDropdown[]> {
     const catalogo_nivelOficina = this.authService.obtenerCatalogoDeLocalStorage(('catalogo_nivelOficina'));
     return of(mapearArregloTipoDropdown(catalogo_nivelOficina, "desc", "id"));
-  }
-
-  exportarArchivo(tipoArchivo: any,): Observable<HttpRespuesta<any>> {
-    const params: HttpParams = new HttpParams().append("servicio", "generarDocumento")
-    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/buscar/rol-generarDocumento`, tipoArchivo,
-      {params});
   }
 
 }
