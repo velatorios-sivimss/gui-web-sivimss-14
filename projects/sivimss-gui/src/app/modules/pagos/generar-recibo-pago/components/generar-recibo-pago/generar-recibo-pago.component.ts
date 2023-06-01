ba2b23fbfb0acef7dd2ca600d24f861fe3b05c83
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-05-31 20:17:03 -0600

    SONAR Generar Recibo

--- projects/sivimss-gui/src/app/modules/pagos/generar-recibo-pago/components/generar-recibo-pago/generar-recibo-pago.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/generar-recibo-pago/components/generar-recibo-pago/generar-recibo-pago.component.ts
@@ -7,7 +7,7 @@ import {FormBuilder, FormGroup} from '@angular/forms';
 import {TipoDropdown} from 'projects/sivimss-gui/src/app/models/tipo-dropdown';
 import {CATALOGOS_DUMMIES} from '../../constants/dummies';
 import {BreadcrumbService} from 'projects/sivimss-gui/src/app/shared/breadcrumb/services/breadcrumb.service';
-import {AlertaService, TipoAlerta} from 'projects/sivimss-gui/src/app/shared/alerta/services/alerta.service';
+import {AlertaService} from 'projects/sivimss-gui/src/app/shared/alerta/services/alerta.service';
 import {LazyLoadEvent} from 'primeng/api';
 import {SERVICIO_BREADCRUMB} from '../../constants/breadcrumb';
 import {GenerarReciboService} from '../../services/generar-recibo-pago.service';
@@ -17,10 +17,9 @@ import {finalize} from "rxjs/operators";
 import {HttpErrorResponse} from "@angular/common/http";
 import {LoaderService} from "../../../../../shared/loader/services/loader.service";
 import {DescargaArchivosService} from "../../../../../services/descarga-archivos.service";
-import {mapearArregloTipoDropdown} from 'projects/sivimss-gui/src/app/utils/funciones';
-import {OpcionesArchivos} from 'projects/sivimss-gui/src/app/models/opciones-archivos.interface';
+import {mapearArregloTipoDropdown, validarUsuarioLogueado} from 'projects/sivimss-gui/src/app/utils/funciones';
 import {MensajesSistemaService} from 'projects/sivimss-gui/src/app/services/mensajes-sistema.service';
-import {obtenerFechaActual} from "../../../../../utils/funciones-fechas";
+import {HttpRespuesta} from "../../../../../models/http-respuesta.interface";
 
 type ListadoRecibo = Required<ReciboPago> & { idPagoBitacora: string }
 
@@ -41,7 +40,7 @@ export class GenerarReciboPagoComponent implements OnInit {
 
   recibosPago: ReciboPago[] = [];
   reciboPagoSeleccionado!: ListadoRecibo;
-  filtroForm!: FormGroup;
+  filtroFormReciboPago!: FormGroup;
   creacionRef!: DynamicDialogRef;
   detalleRef!: DynamicDialogRef;
   modificacionRef!: DynamicDialogRef;
@@ -56,8 +55,6 @@ export class GenerarReciboPagoComponent implements OnInit {
 
   readonly POSICION_CATALOGO_NIVELES: number = 0;
   readonly POSICION_CATALOGO_DELEGACIONES: number = 1;
-  readonly POSICION_CATALOGO_VELATORIOS: number = 2;
-  readonly ERROR_DESCARGA_ARCHIVO: string = "Error al guardar el archivo";
   rolLocalStorage = JSON.parse(localStorage.getItem('usuario') as string);
 
   constructor(
@@ -70,11 +67,9 @@ export class GenerarReciboPagoComponent implements OnInit {
     private router: Router,
     private activatedRoute: ActivatedRoute,
     private cargadorService: LoaderService,
-    private descargaArchivosService: DescargaArchivosService,
     private mensajesSistemaService: MensajesSistemaService
   ) {
   }
-
 
   ngOnInit(): void {
     this.breadcrumbService.actualizar(SERVICIO_BREADCRUMB);
@@ -94,14 +89,14 @@ export class GenerarReciboPagoComponent implements OnInit {
   }
 
   abrirModalReciboPagoTramites(): void {
-    this.router.navigate(['generar-recibo-pago-tramites'], {
+    void this.router.navigate(['generar-recibo-pago-tramites'], {
       relativeTo: this.activatedRoute,
       queryParams: {idPagoBitacora: this.reciboPagoSeleccionado.idPagoBitacora}
     });
   }
 
   async inicializarFiltroForm() {
-    this.filtroForm = this.formBuilder.group({
+    this.filtroFormReciboPago = this.formBuilder.group({
       nivel: [{value: +this.rolLocalStorage.idRol || null, disabled: +this.rolLocalStorage.idRol >= 1}],
       delegacion: [{value: +this.rolLocalStorage.idDelegacion || null, disabled: +this.rolLocalStorage.idRol >= 2}],
       velatorio: [{value: +this.rolLocalStorage.idVelatorio || null, disabled: +this.rolLocalStorage.idRol === 3}],
@@ -114,9 +109,9 @@ export class GenerarReciboPagoComponent implements OnInit {
   }
 
   seleccionarPaginacion(event?: LazyLoadEvent): void {
-
+    if (validarUsuarioLogueado()) return;
     if (event) {
-      this.numPaginaActual = Math.floor((event.first || 0) / (event.rows || 1));
+      this.numPaginaActual = Math.floor((event.first ?? 0) / (event.rows ?? 1));
     }
     if (this.paginacionConFiltrado) {
       this.paginarConFiltros();
@@ -128,34 +123,32 @@ export class GenerarReciboPagoComponent implements OnInit {
   paginar(): void {
     this.cargadorService.activar();
     this.generarReciboService.buscarPorPagina(this.numPaginaActual, this.cantElementosPorPagina)
-      .pipe(finalize(() => this.cargadorService.desactivar()))
-      .subscribe(
-        (respuesta) => {
-          this.recibosPago = respuesta!.datos.content;
-          this.totalElementos = respuesta!.datos.totalElements;
-        },
-        (error: HttpErrorResponse) => {
-          console.error(error);
-          this.alertaService.mostrar(TipoAlerta.Error, error.message);
-        }
-      );
+      .pipe(finalize(() => this.cargadorService.desactivar())).subscribe({
+      next: (respuesta: HttpRespuesta<any>): void => {
+        this.recibosPago = respuesta.datos.content;
+        this.totalElementos = respuesta.datos.totalElements;
+      },
+      error: (error: HttpErrorResponse): void => {
+        console.error(error);
+        this.mensajesSistemaService.mostrarMensajeError(error.message);
+      }
+    });
   }
 
   paginarConFiltros(): void {
     const filtros: FiltrosReciboPago = this.crearSolicitudFiltros();
     this.cargadorService.activar();
     this.generarReciboService.buscarPorFiltros(filtros, this.numPaginaActual, this.cantElementosPorPagina)
-      .pipe(finalize(() => this.cargadorService.desactivar()))
-      .subscribe(
-        (respuesta) => {
-          this.recibosPago = respuesta!.datos.content;
-          this.totalElementos = respuesta!.datos.totalElements;
-        },
-        (error: HttpErrorResponse) => {
-          console.error(error);
-          this.alertaService.mostrar(TipoAlerta.Error, error.message);
-        }
-      );
+      .pipe(finalize(() => this.cargadorService.desactivar())).subscribe({
+      next: (respuesta: HttpRespuesta<any>): void => {
+        this.recibosPago = respuesta.datos.content;
+        this.totalElementos = respuesta.datos.totalElements;
+      },
+      error: (error: HttpErrorResponse): void => {
+        console.error(error);
+        this.mensajesSistemaService.mostrarMensajeError(error.message);
+      }
+    });
   }
 
   buscar(): void {
@@ -166,35 +159,29 @@ export class GenerarReciboPagoComponent implements OnInit {
 
   crearSolicitudFiltros(): FiltrosReciboPago {
     return {
-      idNivel: this.filtroForm.get("nivel")?.value,
-      idDelegacion: this.filtroForm.get("delegacion")?.value,
-      idVelatorio: this.filtroForm.get("velatorio")?.value,
-      claveFolio: this.filtroForm.get("folio")?.value,
-      nomContratante: this.filtroForm.get("nombreContratante")?.value,
-      fecIniODS: this.filtroForm.get("fechaInicial")?.value,
-      fecFinODS: this.filtroForm.get("fechaFinal")?.value,
+      idNivel: this.filtroFormReciboPago.get("nivel")?.value,
+      idDelegacion: this.filtroFormReciboPago.get("delegacion")?.value,
+      idVelatorio: this.filtroFormReciboPago.get("velatorio")?.value,
+      claveFolio: this.filtroFormReciboPago.get("folio")?.value,
+      nomContratante: this.filtroFormReciboPago.get("nombreContratante")?.value,
+      fecIniODS: this.filtroFormReciboPago.get("fechaInicial")?.value,
+      fecFinODS: this.filtroFormReciboPago.get("fechaFinal")?.value,
       rutaNombreReporte: "reportes/generales/ReporteFiltrosRecPagos.jrxml",
       tipoReporte: "pdf"
     }
   }
 
   limpiar(): void {
-    this.filtroForm.reset();
-    this.paginar();
-
+    this.alertaService.limpiar();
+    this.filtroFormReciboPago.reset();
     this.f.nivel.setValue(+this.rolLocalStorage.idRol || null);
-
     if (+this.rolLocalStorage.idRol >= 2) {
       this.f.delegacion.setValue(+this.rolLocalStorage.idDelegacion || null);
     }
-
     if (+this.rolLocalStorage.idRol === 3) {
       this.f.velatorio.setValue(+this.rolLocalStorage.idVelatorio || null);
     }
-  }
-
-  get f() {
-    return this.filtroForm?.controls;
+    this.paginar();
   }
 
   async obtenerVelatorios() {
@@ -208,36 +195,8 @@ export class GenerarReciboPagoComponent implements OnInit {
     );
   }
 
-  guardarListadoPagosPDF() {
-    this.cargadorService.activar();
-    const filtros: FiltrosReciboPago = this.crearSolicitudFiltros();
-    this.descargaArchivosService.descargarArchivo(this.generarReciboService.descargarListadoPDF(filtros)).pipe(
-      finalize(() => this.cargadorService.desactivar())
-    ).subscribe(
-      (respuesta) => {
-        console.log(respuesta)
-      },
-      (error) => {
-        console.log(error)
-      },
-    )
-  }
-
-  guardarListadoPagosExcel() {
-    this.cargadorService.activar();
-    const filtros: FiltrosReciboPago = this.crearSolicitudFiltros();
-    const configuracionArchivo: OpcionesArchivos = {nombreArchivo: "reporte", ext: "xlsx"}
-    this.descargaArchivosService.descargarArchivo(this.generarReciboService.descargarListadoExcel(),
-      configuracionArchivo).pipe(
-      finalize(() => this.cargadorService.desactivar())).subscribe({
-      next: (respuesta): void => {
-        console.log(respuesta)
-      },
-      error: (error): void => {
-        this.mensajesSistemaService.mostrarMensajeError(error.message, this.ERROR_DESCARGA_ARCHIVO);
-        console.log(error)
-      },
-    })
+  get f() {
+    return this.filtroFormReciboPago?.controls;
   }
 
 }
