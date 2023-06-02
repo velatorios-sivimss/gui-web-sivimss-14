757c432993c1b3f02ab213a386ebcdef0e79f9bb
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-06-02 14:48:46 -0600

    Funciones de Descarga de Documentos

--- projects/sivimss-gui/src/app/modules/pagos/generar-recibo-pago/components/detalle-pago-tramites/detalle-pago-tramites.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/generar-recibo-pago/components/detalle-pago-tramites/detalle-pago-tramites.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {DetalleReciboPago} from "../../models/detalleReciboPago.interface";
 import {ActivatedRoute} from "@angular/router";
+import {GenerarReciboService} from "../../services/generar-recibo-pago.service";
+import {MensajesSistemaService} from "../../../../../services/mensajes-sistema.service";
+import {LoaderService} from "../../../../../shared/loader/services/loader.service";
+import {finalize} from "rxjs/operators";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-detalle-pago-tramites',
@@ -16,6 +21,9 @@ export class DetallePagoTramitesComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private generarReciboService: GenerarReciboService,
+    private mensajesSistemaService: MensajesSistemaService,
+    private cargadorService: LoaderService,
   ) {
     this.recibo = this.route.snapshot.data["respuesta"].datos[0];
     this.obtenerValoresFecha();
@@ -36,4 +44,71 @@ export class DetallePagoTramitesComponent implements OnInit {
     const objetoFecha: Date = new Date(+anio, +mes - 1, +dia);
     return objetoFecha.setMinutes(objetoFecha.getMinutes() + objetoFecha.getTimezoneOffset());
   }
+
+  generarExcel(): void {
+    const solicitud = this.generarSolicitudReporte('xls');
+    this.cargadorService.activar();
+    this.generarReciboService.descargarReporte(solicitud).pipe(
+      finalize(() => this.cargadorService.desactivar())
+    ).subscribe({
+      next: (respuesta: Blob): void => {
+        const downloadURL: string = window.URL.createObjectURL(respuesta);
+        const link: HTMLAnchorElement = document.createElement('a');
+        link.href = downloadURL;
+        link.download = `reporte.xlsx`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      },
+      error: (error: HttpErrorResponse): void => {
+        console.error(error)
+      }
+    });
+  }
+
+  generarPDF(): void {
+    const solicitud = this.generarSolicitudReporte();
+    this.cargadorService.activar();
+    this.generarReciboService.descargarReporte(solicitud).pipe(
+      finalize(() => this.cargadorService.desactivar())
+    ).subscribe({
+      next: (respuesta: Blob): void => {
+        const downloadURL: string = window.URL.createObjectURL(respuesta);
+        const link: HTMLAnchorElement = document.createElement('a');
+        link.href = downloadURL;
+        link.download = `reporte.pdf`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      },
+      error: (error: HttpErrorResponse): void => {
+        console.error(error)
+      }
+    });
+  }
+
+  generarSolicitudReporte(tipoReporte: string = "pdf") {
+    return {
+      folio: this.recibo.folio,
+      delegacion: this.recibo.delegacion,
+      velatorio: this.recibo.velatorio,
+      lugar: "Mexico CDMX",
+      fecha: `${this.dia} de ${this.colocarTitleCase(this.mes)} del ${this.anio}`,
+      recibimos: this.recibo.recibimos,
+      cantidad: this.recibo.cantidad,
+      tramites: this.recibo.canTramites,
+      descTramites: this.recibo.descTramites,
+      derechos: this.recibo.canDerechos,
+      descDerechos: this.recibo.descDerechos,
+      total: this.recibo.canSuma,
+      totalFinal: this.recibo.canTotal,
+      rutaNombreReporte: this.recibo.rutaNombreReporte,
+      tipoReporte: tipoReporte
+    }
+  }
+
+  colocarTitleCase(cadena: string): string {
+    return cadena.split(" ").map((l: string) => l[0].toUpperCase() + l.substr(1)).join(" ");
+  }
+
 }
