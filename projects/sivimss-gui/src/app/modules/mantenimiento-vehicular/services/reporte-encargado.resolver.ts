204166c0f6200c0457defb87194e3dddec2acf85
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-06-02 07:44:48 -0600

    Carga de Catalogos Aux por Resolver

---
+++ projects/sivimss-gui/src/app/modules/mantenimiento-vehicular/services/reporte-encargado.resolver.ts
@@ -0,0 +1,20 @@
+import {Injectable} from "@angular/core";
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from "@angular/router";
+import {MantenimientoVehicularService} from "./mantenimiento-vehicular.service";
+import {forkJoin, Observable} from "rxjs";
+
+@Injectable()
+export class ReporteEncargadoResolver implements Resolve<any> {
+
+  constructor(private mantenimientoVehicularService: MantenimientoVehicularService) {
+  }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+    const niveles$ = this.mantenimientoVehicularService.obtenerCatalogoNiveles();
+    const delegaciones$ = this.mantenimientoVehicularService.obtenerCatalogoDelegaciones();
+    const placas$ = this.mantenimientoVehicularService.obtenerCatalogoPlacas();
+    const catalogoBase$ = this.mantenimientoVehicularService.obtenerCatalogoReporteEncargado();
+
+    return forkJoin([niveles$, delegaciones$, placas$, catalogoBase$]);
+  }
+}
