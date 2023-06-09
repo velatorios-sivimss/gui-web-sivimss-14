d475d3cde0b39a27e411402f6719b8e91f6ea3a2
GRUPOPM\mparedes <martin.paredes@people-media.com.mx>
2023-06-09 10:59:21 -0600

    Ajustes iniciales CU027

--- projects/sivimss-gui/src/app/modules/convenios-prevision-funeraria/components/convenios-prevision-funeraria/convenios-prevision-funeraria.component.ts
+++ projects/sivimss-gui/src/app/modules/convenios-prevision-funeraria/components/convenios-prevision-funeraria/convenios-prevision-funeraria.component.ts
@@ -1,21 +1,21 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {OverlayPanel} from "primeng/overlaypanel";
-import {FormBuilder, FormGroup} from "@angular/forms";
-import {DIEZ_ELEMENTOS_POR_PAGINA} from "../../../../utils/constantes";
-import {BreadcrumbService} from "../../../../shared/breadcrumb/services/breadcrumb.service";
-import {AlertaService, TipoAlerta} from "../../../../shared/alerta/services/alerta.service";
-import {DialogService, DynamicDialogRef} from "primeng/dynamicdialog";
-import {SERVICIO_BREADCRUMB} from "../../constants/breadcrumb";
-import {TipoDropdown} from "../../../../models/tipo-dropdown";
-import {LazyLoadEvent} from "primeng/api";
-import {ConveniosPrevisionFunerariaInterface} from "../../models/convenios-prevision-funeraria.interface";
-import {BeneficiarioInterface} from "../../models/beneficiario.interface";
-import {AfiliadoInterface} from "../../models/afiliado.interface";
-import {VigenciaConvenioInterface} from "../../models/vigencia-convenio.interface";
-import {FacturaConvenioInterface} from "../../models/factura-convenio.interface";
-import {SinisestroInterface} from "../../models/sinisestro.interface";
-import {finalize} from "rxjs/operators";
-import {HttpErrorResponse} from "@angular/common/http";
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { OverlayPanel } from "primeng/overlaypanel";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { DIEZ_ELEMENTOS_POR_PAGINA } from "../../../../utils/constantes";
+import { BreadcrumbService } from "../../../../shared/breadcrumb/services/breadcrumb.service";
+import { AlertaService, TipoAlerta } from "../../../../shared/alerta/services/alerta.service";
+import { DialogService, DynamicDialogRef } from "primeng/dynamicdialog";
+import { SERVICIO_BREADCRUMB } from "../../constants/breadcrumb";
+import { TipoDropdown } from "../../../../models/tipo-dropdown";
+import { LazyLoadEvent } from "primeng/api";
+import { ConveniosPrevisionFunerariaInterface } from "../../models/convenios-prevision-funeraria.interface";
+import { BeneficiarioInterface } from "../../models/beneficiario.interface";
+import { AfiliadoInterface } from "../../models/afiliado.interface";
+import { VigenciaConvenioInterface } from "../../models/vigencia-convenio.interface";
+import { FacturaConvenioInterface } from "../../models/factura-convenio.interface";
+import { SinisestroInterface } from "../../models/sinisestro.interface";
+import { finalize } from "rxjs/operators";
+import { HttpErrorResponse } from "@angular/common/http";
 import { ConsultaConveniosService } from '../../services/consulta-convenios.service';
 import { LoaderService } from 'projects/sivimss-gui/src/app/shared/loader/services/loader.service';
 import { FiltrosConvenioInterface } from '../../models/filtros-convenio.interface';
@@ -25,6 +25,9 @@ import { OpcionesArchivos } from 'projects/sivimss-gui/src/app/models/opciones-a
 import {
   DetalleConvenioPrevisionFunerariaComponent
 } from "../detalle-convenio-prevision-funeraria/detalle-convenio-prevision-funeraria.component";
+import { Router } from '@angular/router';
+import { validarAlMenosUnCampoConValor } from 'projects/sivimss-gui/src/app/utils/funciones';
+import { HttpRespuesta } from 'projects/sivimss-gui/src/app/models/http-respuesta.interface';
 
 
 @Component({
@@ -69,7 +72,7 @@ export class ConsultaConveniosComponent implements OnInit {
 
   convenioSeleccionado: ConveniosPrevisionFunerariaInterface = {};
   readonly ERROR_DESCARGA_ARCHIVO: string = "Error al guardar el archivo";
-  
+
   detalleRef!: DynamicDialogRef;
 
   constructor(
@@ -80,7 +83,8 @@ export class ConsultaConveniosComponent implements OnInit {
     private consultaConvenioService: ConsultaConveniosService,
     private cargadorService: LoaderService,
     private descargaArchivosService: DescargaArchivosService,
-    private mensajesSistemaService: MensajesSistemaService
+    private mensajesSistemaService: MensajesSistemaService,
+    private readonly router: Router,
   ) { }
 
   ngOnInit(): void {
@@ -88,7 +92,7 @@ export class ConsultaConveniosComponent implements OnInit {
     this.inicializarFiltroForm();
   }
 
-  actualizarBreadcrumb(): void{
+  actualizarBreadcrumb(): void {
     /*Cambiar la imagen de Administración de catálogos*/
     this.breadcrumbService.actualizar(SERVICIO_BREADCRUMB);
   }
@@ -96,11 +100,11 @@ export class ConsultaConveniosComponent implements OnInit {
   inicializarFiltroForm(): void {
 
     this.filtroForm = this.formBuilder.group({
-      folioConvenio: [{value: null, disabled:false}],
-      rfc: [{value: null, disabled:false}],
-      nombre: [{value: null, disabled:false}],
-      curp: [{value: null, disabled:false}],
-      estatusConvenio: [{value: null, disabled:false}]
+      folioConvenio: [{ value: null, disabled: false }, Validators.maxLength(12)],
+      rfc: [{ value: null, disabled: false }, Validators.maxLength(13)],
+      nombre: [{ value: null, disabled: false }, Validators.maxLength(75)],
+      curp: [{ value: null, disabled: false }, Validators.maxLength(18)],
+      estatusConvenio: [{ value: null, disabled: false }]
     });
   }
 
@@ -108,16 +112,16 @@ export class ConsultaConveniosComponent implements OnInit {
     this.cargadorService.activar();
     this.consultaConvenioService.buscarPorPagina(this.numPaginaActual, this.cantElementosPorPagina)
       .pipe(finalize(() => this.cargadorService.desactivar()))
-      .subscribe(
-        (respuesta) => {
-          this.convenioPrevision = respuesta!.datos.content;
-          this.totalElementos = respuesta!.datos.totalElements;
+      .subscribe({
+        next: (respuesta: HttpRespuesta<any>) => {
+          this.convenioPrevision = respuesta.datos?.content;
+          this.totalElementos = respuesta.datos?.totalElements;
         },
-        (error: HttpErrorResponse) => {
+        error: (error: HttpErrorResponse) => {
           console.error(error);
           this.alertaService.mostrar(TipoAlerta.Error, error.message);
         }
-      );
+      });
   }
 
   paginarConFiltros(): void {
@@ -125,16 +129,16 @@ export class ConsultaConveniosComponent implements OnInit {
     this.cargadorService.activar();
     this.consultaConvenioService.buscarPorFiltros(filtros, this.numPaginaActual, this.cantElementosPorPagina)
       .pipe(finalize(() => this.cargadorService.desactivar()))
-      .subscribe(
-        (respuesta) => {
-          this.convenioPrevision = respuesta!.datos.content;
-          this.totalElementos = respuesta!.datos.totalElements;
+      .subscribe({
+        next: (respuesta: HttpRespuesta<any>) => {
+          this.convenioPrevision = respuesta.datos?.content;
+          this.totalElementos = respuesta.datos?.totalElements;
         },
-        (error: HttpErrorResponse) => {
+        error: (error: HttpErrorResponse) => {
           console.error(error);
           this.alertaService.mostrar(TipoAlerta.Error, error.message);
         }
-      );
+      });
   }
 
   crearSolicitudFiltros(): FiltrosConvenioInterface {
@@ -156,14 +160,14 @@ export class ConsultaConveniosComponent implements OnInit {
           fechaContratacion: "01/01/2021",
           fechaVigenciaInicio: "01/01/2021",
           fechaVigenciaFin: "01/01/2022",
-          cantidadBeneficiarios: this.devolverBeneficiarios( [{nombre:"Juan"}]),
+          cantidadBeneficiarios: this.devolverBeneficiarios([{ nombre: "Juan" }]),
           situacion: "N/A",
-          factura:"DOC-00001",
+          factura: "DOC-00001",
           importeConvenio: 50,
-          estatus:0,
-          beneficiario:[
+          estatus: 0,
+          beneficiario: [
             {
-              nombre:"Juan"
+              nombre: "Juan"
             }
           ]
         },
@@ -172,17 +176,17 @@ export class ConsultaConveniosComponent implements OnInit {
           fechaContratacion: "01/01/2021",
           fechaVigenciaInicio: "01/01/2021",
           fechaVigenciaFin: "01/01/2022",
-          cantidadBeneficiarios: this.devolverBeneficiarios( [{nombre:"Juan"},{nombre:"Juan"}]),
+          cantidadBeneficiarios: this.devolverBeneficiarios([{ nombre: "Juan" }, { nombre: "Juan" }]),
           situacion: "N/A",
-          factura:"DOC-00001",
+          factura: "DOC-00001",
           importeConvenio: 50,
-          estatus:1,
-          beneficiario:[
+          estatus: 1,
+          beneficiario: [
             {
-              nombre:"Juan"
+              nombre: "Juan"
             },
             {
-              nombre:"Juan"
+              nombre: "Juan"
             }
           ]
         },
@@ -191,77 +195,77 @@ export class ConsultaConveniosComponent implements OnInit {
           fechaContratacion: "01/01/2021",
           fechaVigenciaInicio: "01/01/2021",
           fechaVigenciaFin: "01/01/2022",
-          cantidadBeneficiarios: this.devolverBeneficiarios( [{nombre:"Juan"},{nombre:"Juan"},{nombre:"Juan"}]),
+          cantidadBeneficiarios: this.devolverBeneficiarios([{ nombre: "Juan" }, { nombre: "Juan" }, { nombre: "Juan" }]),
           situacion: "N/A",
-          factura:"DOC-00001",
+          factura: "DOC-00001",
           importeConvenio: 50,
-          estatus:2,
-          beneficiario:[
+          estatus: 2,
+          beneficiario: [
             {
-              nombre:"Juan"
+              nombre: "Juan"
             },
             {
-              nombre:"Juan"
+              nombre: "Juan"
             },
             {
-              nombre:"Juan"
+              nombre: "Juan"
             }
           ]
         }
       ];
       this.datosAfiliado = [
         {
-          rfc:"VEVIAA84751T7",
-          velatorio:1,
-          descVelatorio:"No. 01 Doctores",
+          rfc: "VEVIAA84751T7",
+          velatorio: 1,
+          descVelatorio: "No. 01 Doctores",
           afiliado: "Joel Durán Mendoza",
           rfcTitular: "DUMEJO8475T7",
-          edad:34,
-          fechaNacimiento:"01/01/2021",
-          genero:"Masculuno",
-          correoElectronico:"jodu87@gmail.com"
+          edad: 34,
+          fechaNacimiento: "01/01/2021",
+          genero: "Masculuno",
+          correoElectronico: "jodu87@gmail.com"
         },
         {
-          rfc:"VEVIAA84751T7",
-          velatorio:1,
-          descVelatorio:"No. 01 Doctores",
+          rfc: "VEVIAA84751T7",
+          velatorio: 1,
+          descVelatorio: "No. 01 Doctores",
           afiliado: "Joel Durán Mendoza",
           rfcTitular: "DUMEJO8475T7",
-          fechaNacimiento:"01/01/2021",
-          edad:34,
-          genero:"Masculuno",
-          correoElectronico:"jodu87@gmail.com"
+          fechaNacimiento: "01/01/2021",
+          edad: 34,
+          genero: "Masculuno",
+          correoElectronico: "jodu87@gmail.com"
         }
       ];
       this.vigenciaConvenio = [
         {
-          convenio:"DOC-000001",
-          fechaInicio:"01/01/2022",
-          fechaFin:"01/01/2022",
-          fechaRenovacion:"01/01/2022"
+          convenio: "DOC-000001",
+          fechaInicio: "01/01/2022",
+          fechaFin: "01/01/2022",
+          fechaRenovacion: "01/01/2022"
         },
         {
-          convenio:"DOC-000001",
-          fechaInicio:"01/01/2022",
-          fechaFin:"01/01/2022",
-          fechaRenovacion:"01/01/2022"
+          convenio: "DOC-000001",
+          fechaInicio: "01/01/2022",
+          fechaFin: "01/01/2022",
+          fechaRenovacion: "01/01/2022"
         }
       ];
       this.facturaConvenio = [
         {
-          factura:"DOC-001",
-          uuid:"ABC001",
-          fecha:"01/01/2022",
-          rfc:"ABC12345",
+          factura: "DOC-001",
+          uuid: "ABC001",
+          fecha: "01/01/2022",
+          rfc: "ABC12345",
           cliente: "Genetics SA de CV",
           total: 220002,
           estatus: true
         },
         {
-          factura:"DOC-001",
-          uuid:"ABC001",
-          fecha:"01/01/2022",
-          rfc:"ABC12345",
+          factura: "DOC-001",
+          uuid: "ABC001",
+          fecha: "01/01/2022",
+          rfc: "ABC12345",
           cliente: "Genetics SA de CV",
           total: 220002,
           estatus: false
@@ -271,7 +275,7 @@ export class ConsultaConveniosComponent implements OnInit {
         {
           nombre: "Sebastián",
           primerApellido: "Gómez",
-          fecha:"01/01/2022",
+          fecha: "01/01/2022",
           edad: 23,
           parentesco: 1,
           descParentesco: "Hijo"
@@ -281,86 +285,130 @@ export class ConsultaConveniosComponent implements OnInit {
         {
           velatorio: 1,
           descVelatorio: "No. 01 Doctores",
-          fechaSiniestro:"01/01/2022",
-          folio:"DOC-0111",
-          nota:"No existen notas previas",
-          finado:"Angel",
+          fechaSiniestro: "01/01/2022",
+          folio: "DOC-0111",
+          nota: "No existen notas previas",
+          finado: "Angel",
           parentesco: 1,
-          descPrentesco:"Abuelo",
+          descPrentesco: "Abuelo",
           velatorioOringe: 2,
           descVelatorioOrigen: "No. 02 Malvin",
           importe: 6000
         }
       ];
       this.totalElementos = this.convenioPrevision.length;
-    },0)
+    }, 0)
 
 
   }
 
-  agregarConvenio(): void {
-
-  }
+  agregarConvenio(): void { }
 
   devolverBeneficiarios(beneficiario: BeneficiarioInterface[]): number {
     return beneficiario.length;
   }
 
   abrirModalDetalleConvenio(convenio: ConveniosPrevisionFunerariaInterface): void {
-    this.detalleRef = this.dialogService.open( DetalleConvenioPrevisionFunerariaComponent,{
-      header:"Detalle del convenio",
-      width:"920px",
+    this.detalleRef = this.dialogService.open(DetalleConvenioPrevisionFunerariaComponent, {
+      header: "Detalle del convenio",
+      width: "920px",
       data: convenio,
     })
   }
 
   buscar(): void {
+    if (validarAlMenosUnCampoConValor(this.filtroForm.value)) {
+      this.paginar_();
+    } else {
+      this.ff.folioConvenio.setValidators(Validators.required);
+      this.ff.folioConvenio.updateValueAndValidity();
+      this.ff.rfc.setValidators(Validators.required);
+      this.ff.rfc.updateValueAndValidity();
+      this.ff.nombre.setValidators(Validators.required);
+      this.ff.nombre.updateValueAndValidity();
+      this.ff.curp.setValidators(Validators.required);
+      this.ff.curp.updateValueAndValidity();
+      this.ff.estatusConvenio.setValidators(Validators.required);
+      this.ff.estatusConvenio.updateValueAndValidity();
+      this.filtroForm.markAllAsTouched();
 
+      this.alertaService.mostrar(TipoAlerta.Precaucion, 'Selecciona por favor un criterio de búsqueda.');
+    }
+  }
+
+  paginar_(event?: LazyLoadEvent): void {
+    if (event?.first !== undefined && event.rows !== undefined) {
+      this.numPaginaActual = Math.floor(event.first / event.rows);
+    } else {
+      this.numPaginaActual = 0;
+    }
+    this.buscarPorFiltros();
+  }
+
+  buscarPorFiltros(): void {
+    this.consultaConvenioService.buscarPorFiltros(this.obtenerObjetoParaFiltrado(), this.numPaginaActual, this.cantElementosPorPagina).subscribe({
+      next: (respuesta: HttpRespuesta<any>) => {
+        this.convenioPrevision = respuesta.datos.content;
+        this.totalElementos = respuesta.datos.totalElements;
+      },
+      error: (error: HttpErrorResponse) => {
+        console.error(error);
+        this.alertaService.mostrar(TipoAlerta.Error, error.message);
+      }
+    });
+  }
+
+  obtenerObjetoParaFiltrado(): any {
+    return {
+      ...this.filtroForm.value,
+    };
+  }
+
+  cancelar(): void {
+    this.router.navigate(["/"]).then(() => { }).catch(() => { });
   }
 
   limpiar(): void {
     this.filtroForm.reset();
   }
 
-  abrirPanel(event:MouseEvent,convenio:ConveniosPrevisionFunerariaInterface):void{
+  abrirPanel(event: MouseEvent, convenio: ConveniosPrevisionFunerariaInterface): void {
     this.convenioSeleccionado = convenio;
     this.overlayPanel.toggle(event);
   }
 
-  get ff(){
+  get ff() {
     return this.filtroForm.controls;
   }
 
   guardarListadoPagaresPDF() {
     this.cargadorService.activar();
-    const filtros: FiltrosConvenioInterface = this.crearSolicitudFiltros();
     this.descargaArchivosService.descargarArchivo(this.consultaConvenioService.descargarListadoPagaresPDF()).pipe(
       finalize(() => this.cargadorService.desactivar())
-    ).subscribe(
-      (respuesta) => {
+    ).subscribe({
+      next: (respuesta: any) => {
         console.log(respuesta)
       },
-      (error) => {
+      error: (error: HttpErrorResponse) => {
         console.log(error)
       },
-    )
+    });
   }
 
   guardarListadoPagaresExcel() {
     this.cargadorService.activar();
-    const filtros: FiltrosConvenioInterface = this.crearSolicitudFiltros();
-    const configuracionArchivo: OpcionesArchivos = {nombreArchivo: "reporte", ext: "xlsx"}
+    const configuracionArchivo: OpcionesArchivos = { nombreArchivo: "reporte", ext: "xlsx" }
     this.descargaArchivosService.descargarArchivo(this.consultaConvenioService.descargarListadoPagaresExcel(),
-    configuracionArchivo).pipe(
-      finalize(() => this.cargadorService.desactivar())).subscribe({
-      next: (respuesta): void => {
-        console.log(respuesta)
-      },
-      error: (error): void => {
-        this.mensajesSistemaService.mostrarMensajeError(error.message, this.ERROR_DESCARGA_ARCHIVO);
-        console.log(error)
-      },
-    })
+      configuracionArchivo).pipe(
+        finalize(() => this.cargadorService.desactivar())).subscribe({
+          next: (respuesta: any) => {
+            console.log(respuesta)
+          },
+          error: (error): void => {
+            this.mensajesSistemaService.mostrarMensajeError(error.message, this.ERROR_DESCARGA_ARCHIVO);
+            console.log(error)
+          },
+        })
   }
 
 }
