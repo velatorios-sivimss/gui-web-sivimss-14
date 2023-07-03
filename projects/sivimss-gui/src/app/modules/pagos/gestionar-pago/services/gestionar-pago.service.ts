dc88f887934e754af52f6c723c77a98bac694fbb
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-07-03 12:04:32 -0600

    Base service y base listado

--- projects/sivimss-gui/src/app/modules/pagos/gestionar-pago/services/gestionar-pago.service.ts
+++ projects/sivimss-gui/src/app/modules/pagos/gestionar-pago/services/gestionar-pago.service.ts
@@ -1,8 +1,15 @@
 import {Injectable} from '@angular/core';
+import {BaseService} from "../../../../utils/base-service";
+import {HttpRespuesta} from "../../../../models/http-respuesta.interface";
+import {HttpClient} from "@angular/common/http";
+import {AutenticacionService} from "../../../../services/autenticacion.service";
+import {environment} from "../../../../../environments/environment";
 
 @Injectable()
-export class GestionarPagoService {
+export class GestionarPagoService extends BaseService<HttpRespuesta<any>, any> {
 
-  constructor() {
+  constructor(override _http: HttpClient, private authService: AutenticacionService) {
+    super(_http, `${environment.api.mssivimss}`, '', '', 67,
+      '', '', '');
   }
 }
