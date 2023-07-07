c8e9169dc11a11f36b2ec2dc99cc97a383c16cc7
LeandroPeopleMedia <107154425+LeandroPeopleMedia@users.noreply.github.com>
2023-07-06 19:59:11 -0600

    datis contratante

--- projects/sivimss-gui/src/app/modules/ordenes-servicio/components/modificar-datos-contratante/modificar-datos-contratante.component.ts
+++ projects/sivimss-gui/src/app/modules/ordenes-servicio/components/modificar-datos-contratante/modificar-datos-contratante.component.ts
@@ -20,8 +20,10 @@ import { CaracteristicasDelPresupuestoInterface } from '../../models/Caracterist
 import { DetallePresupuestoInterface } from '../../models/DetallePresupuesto.interface';
 import { InformacionServicioInterface } from '../../models/InformacionServicio.interface';
 import { InformacionServicioVelacionInterface } from '../../models/InformacionServicioVelacion.interface';
-import { GenerarOrdenServicioService } from '../../services/generar-orden-servicio.service';
 import { MensajesSistemaService } from 'projects/sivimss-gui/src/app/services/mensajes-sistema.service';
+
+import { nacionalidad, sexo } from '../../constants/catalogos-complementarios';
+import { ConfirmacionServicio } from '../../../renovacion-extemporanea/models/convenios-prevision.interface';
 import {
   AlertaService,
   TipoAlerta,
@@ -34,8 +36,14 @@ import { finalize } from 'rxjs';
 
 import * as moment from 'moment';
 import { UsuarioEnSesion } from '../../../../models/usuario-en-sesion.interface';
+import {
+  PATRON_CORREO,
+  PATRON_CURP,
+  PATRON_RFC,
+} from '../../../../utils/constantes';
 import { ActivatedRoute } from '@angular/router';
 import { ActualizarOrdenServicioService } from '../../services/actualizar-orden-servicio.service';
+import { TipoDropdown } from 'projects/sivimss-gui/src/app/models/tipo-dropdown';
 
 @Component({
   selector: 'app-modificar-datos-contratante',
@@ -45,6 +53,12 @@ import { ActualizarOrdenServicioService } from '../../services/actualizar-orden-
 export class ModificarDatosContratanteComponent implements OnInit {
   @Output()
   seleccionarEtapa: EventEmitter<number> = new EventEmitter<number>();
+  @Output()
+  confirmacionAceptar = new EventEmitter<ConfirmacionServicio>();
+
+  readonly POSICION_PAIS = 0;
+  readonly POSICION_ESTADO = 1;
+  readonly POSICION_PARENTESCO = 2;
 
   altaODS: AltaODSInterface = {} as AltaODSInterface;
   contratante: ContratanteInterface = {} as ContratanteInterface;
@@ -76,6 +90,19 @@ export class ModificarDatosContratanteComponent implements OnInit {
   ocultarFolioEstatus: boolean = true;
   form!: FormGroup;
   datosDetalle: any = [];
+  tipoSexo: TipoDropdown[] = sexo;
+  muestraOtroSexo: boolean = false;
+  nacionalidad: TipoDropdown[] = nacionalidad;
+  radonlyMatricula: boolean = false;
+
+  estado!: TipoDropdown[];
+  pais!: TipoDropdown[];
+  parentesco!: TipoDropdown[];
+  fechaActual = new Date();
+
+  idPersona: number | null = null;
+  idContratante: number | null = null;
+  idDomicilio: number | null = null;
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly dialogService: DialogService,
@@ -164,6 +191,7 @@ export class ModificarDatosContratanteComponent implements OnInit {
 
   inicializarForm(datos: any, idODS: number, tipoODS: number) {
     console.log('llego despues', datos);
+
     this.form = this.formBuilder.group({
       datosContratante: this.formBuilder.group({
         matricula: [
@@ -173,19 +201,25 @@ export class ModificarDatosContratanteComponent implements OnInit {
           },
           [Validators.required],
         ],
+        matriculaCheck: [
+          {
+            value: datos.contratante.matriculaCheck,
+            disabled: false,
+          },
+        ],
         rfc: [
           {
             value: datos.contratante.rfc,
             disabled: false,
           },
-          [Validators.required],
+          [Validators.pattern(PATRON_RFC)],
         ],
         curp: [
           {
             value: datos.contratante.curp,
             disabled: false,
           },
-          [Validators.required],
+          [Validators.required, Validators.pattern(PATRON_CURP)],
         ],
         nombre: [
           {
@@ -210,108 +244,393 @@ export class ModificarDatosContratanteComponent implements OnInit {
         ],
         fechaNacimiento: [
           {
-            value: datos.contratante.fechaNac,
-            disabled: false,
+            value: datos.contratante.fechaNacimiento,
+            disabled: true,
           },
           [Validators.required],
         ],
         sexo: [
           {
-            value: null,
+            value: datos.contratante.sexo,
             disabled: false,
           },
           [Validators.required],
         ],
+        otroTipoSexo: [
+          {
+            value: datos.contratante.otroTipoSexo,
+            disabled: false,
+          },
+        ],
         nacionalidad: [
           {
-            value: null,
+            value: datos.contratante.nacionalidad,
             disabled: false,
           },
           [Validators.required],
         ],
         lugarNacimiento: [
           {
-            value: null,
+            value: datos.contratante.lugarNacimiento,
             disabled: false,
           },
-          [Validators.required],
+          [],
+        ],
+        paisNacimiento: [
+          {
+            value: datos.contratante.paisNacimiento,
+            disabled: false,
+          },
         ],
         telefono: [
           {
-            value: null,
+            value: datos.contratante.telefono,
             disabled: false,
           },
           [Validators.required],
         ],
         correoElectronico: [
           {
-            value: null,
+            value: datos.contratante.correoElectronico,
             disabled: false,
           },
-          [Validators.required],
+          [Validators.required, Validators.pattern(PATRON_CORREO)],
         ],
         parentesco: [
           {
-            value: null,
+            value: datos.contratante.parentesco,
             disabled: false,
           },
           [Validators.required],
         ],
       }),
       direccion: this.formBuilder.group({
-        direccion: [
-          {
-            value: null,
-            disabled: false,
-          },
-          [Validators.required],
-        ],
         calle: [
           {
-            value: null,
+            value: datos.cp.calle,
             disabled: false,
           },
           [Validators.required],
         ],
         noExterior: [
           {
-            value: null,
+            value: datos.cp.noExterior,
             disabled: false,
           },
           [Validators.required],
         ],
         noInterior: [
           {
-            value: null,
+            value: datos.cp.noInterior,
             disabled: false,
           },
-          [Validators.required],
+          [],
         ],
+        cp: [{ value: datos.cp.cp, disabled: false }, [Validators.required]],
         colonia: [
           {
-            value: null,
+            value: datos.cp.colonia,
             disabled: false,
           },
           [Validators.required],
         ],
         municipio: [
           {
-            value: null,
-            disabled: false,
+            value: datos.cp.municipio,
+            disabled: true,
           },
           [Validators.required],
         ],
         estado: [
           {
-            value: null,
-            disabled: false,
+            value: datos.cp.estado,
+            disabled: true,
           },
           [Validators.required],
         ],
       }),
     });
+    // this.cambiarValidacion();
+    //this.idContratante = datosEtapaContratante.datosContratante.idContratante;
+    //this.idPersona = datosEtapaContratante.datosContratante.idPersona;
   }
 
+  cambiarValidacion(): void {
+    this.radonlyMatricula = false;
+    if (!this.datosContratante.matriculaCheck.value) {
+      this.radonlyMatricula = true;
+      this.datosContratante.matricula.clearValidators();
+      this.datosContratante.matricula.patchValue(null);
+      return;
+    }
+
+    this.datosContratante.matricula.setValidators(Validators.required);
+    this.datosContratante.matricula.patchValue(
+      this.datosContratante.matricula.value
+    );
+  }
+  consultarMatriculaSiap(): void {
+    this.loaderService.activar();
+
+    this.gestionarOrdenServicioService
+      .consultarMatriculaSiap(this.datosContratante.matricula.value)
+      .pipe(finalize(() => this.loaderService.desactivar()))
+      .subscribe(
+        (respuesta: HttpRespuesta<any>) => {
+          if (!respuesta.datos) {
+            this.alertaService.mostrar(
+              TipoAlerta.Precaucion,
+              this.mensajesSistemaService.obtenerMensajeSistemaPorId(70)
+            );
+            this.datosContratante.matricula.setValue(null);
+          }
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      );
+  }
+  consultarCURP(): void {
+    if (!this.datosContratante.curp.value) {
+      return;
+    }
+    this.loaderService.activar();
+    this.gestionarOrdenServicioService
+      .consultarCURP(this.datosContratante.curp.value)
+      .pipe(finalize(() => this.loaderService.desactivar()))
+      .subscribe(
+        (respuesta: HttpRespuesta<any>) => {
+          if (respuesta.datos) {
+            console.log('curp', respuesta);
+            if (respuesta.mensaje.includes('Externo')) {
+              this.idPersona = null;
+              this.idContratante = null;
+              const [dia, mes, anio] = respuesta.datos.fechNac.split('/');
+              const fecha = new Date(anio + '/' + mes + '/' + dia);
+              this.datosContratante.nombre.setValue(respuesta.datos.nombre);
+              this.datosContratante.primerApellido.setValue(
+                respuesta.datos.apellido1
+              );
+              this.datosContratante.segundoApellido.setValue(
+                respuesta.datos.apellido2
+              );
+              this.datosContratante.fechaNacimiento.setValue(fecha);
+              if (respuesta.datos.sexo.includes('HOMBRE')) {
+                this.datosContratante.sexo.setValue(2);
+              }
+              if (respuesta.datos.sexo.includes('MUJER')) {
+                this.datosContratante.sexo.setValue(1);
+              }
+              if (
+                respuesta.datos.desEntidadNac.includes('MEXICO') ||
+                respuesta.datos.desEntidadNac.includes('MEX')
+              ) {
+                this.datosContratante.nacionalidad.setValue(1);
+              } else {
+                this.datosContratante.nacionalidad.setValue(2);
+              }
+            } else {
+              let datos = respuesta.datos[0];
+              this.idPersona = datos.idPersona;
+              this.idContratante = datos.idContratante;
+              let [anio, mes, dia] = datos.fechaNac.split('-');
+              dia = dia.substr(0, 2);
+              const fecha = new Date(anio + '/' + mes + '/' + dia);
+              this.datosContratante.nombre.setValue(datos.nombre);
+              this.datosContratante.primerApellido.setValue(
+                respuesta.primerApellido
+              );
+              this.datosContratante.segundoApellido.setValue(
+                datos.segundoApellido
+              );
+              this.datosContratante.fechaNacimiento.setValue(fecha);
+
+              this.datosContratante.sexo.setValue(+respuesta.datos[0].sexo);
+              if (datos.idPais == 119) {
+                this.datosContratante.nacionalidad.setValue(1);
+              } else {
+                this.datosContratante.nacionalidad.setValue(2);
+              }
+
+              this.datosContratante.rfc.setValue(datos.rfc);
+              console.log(datos);
+              this.datosContratante.paisNacimiento.setValue(
+                Number(datos.idPais)
+              );
+              this.datosContratante.lugarNacimiento.setValue(
+                Number(datos.idEstado)
+              );
+
+              this.datosContratante.telefono.setValue(datos.telefono);
+              this.datosContratante.correoElectronico.setValue(datos.correo);
+
+              this.direccion.colonia.setValue(datos.colonia);
+              this.direccion.municipio.setValue(datos.municipio);
+              this.direccion.estado.setValue(datos.estado);
+              this.direccion.cp.setValue(datos.cp);
+              this.direccion.colonia.setValue(datos.colonia);
+              this.direccion.calle.setValue(datos.calle);
+
+              this.direccion.noInterior.setValue(datos.numExterior);
+              this.direccion.noExterior.setValue(datos.numInterior);
+              this.idDomicilio = datos.idDomicilio;
+            }
+            return;
+          }
+          this.limpiarConsultaDatosPersonales();
+          this.alertaService.mostrar(
+            TipoAlerta.Precaucion,
+            this.mensajesSistemaService.obtenerMensajeSistemaPorId(
+              parseInt(respuesta.mensaje)
+            )
+          );
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      );
+  }
+
+  limpiarConsultaDatosPersonales(): void {
+    this.datosContratante.nombre.patchValue(null);
+    this.datosContratante.primerApellido.patchValue(null);
+    this.datosContratante.segundoApellido.patchValue(null);
+    this.datosContratante.fechaNacimiento.patchValue(null);
+    this.datosContratante.sexo.reset();
+    this.datosContratante.nacionalidad.reset();
+  }
+
+  consultarRFC(): void {
+    if (!this.datosContratante.rfc.value) {
+      return;
+    }
+    this.loaderService.activar();
+    this.gestionarOrdenServicioService
+      .consultarRFC(this.datosContratante.rfc.value)
+      .pipe(finalize(() => this.loaderService.desactivar()))
+      .subscribe(
+        (respuesta: HttpRespuesta<any>) => {
+          console.log(respuesta);
+          if (respuesta.datos.length > 0) {
+            let datos = respuesta.datos[0];
+            this.idPersona = datos.idPersona;
+            this.idContratante = datos.idContratante;
+            let [anio, mes, dia] = datos.fechaNac.split('-');
+            dia = dia.substr(0, 2);
+            const fecha = new Date(anio + '/' + mes + '/' + dia);
+            this.datosContratante.nombre.setValue(datos.nombre);
+            this.datosContratante.primerApellido.setValue(datos.primerApellido);
+
+            this.datosContratante.segundoApellido.setValue(
+              datos.segundoApellido
+            );
+            this.datosContratante.fechaNacimiento.setValue(fecha);
+
+            this.datosContratante.sexo.setValue(+respuesta.datos[0].sexo);
+            if (datos.idPais == 119) {
+              this.datosContratante.nacionalidad.setValue(1);
+            } else {
+              this.datosContratante.nacionalidad.setValue(2);
+            }
+
+            this.datosContratante.rfc.setValue(datos.rfc);
+
+            this.datosContratante.paisNacimiento.setValue(Number(datos.idPais));
+            this.datosContratante.lugarNacimiento.setValue(
+              Number(datos.idEstado)
+            );
+
+            this.datosContratante.telefono.setValue(datos.telefono);
+            this.datosContratante.correoElectronico.setValue(datos.correo);
+            this.datosContratante.curp.setValue(datos.curp);
+            this.direccion.colonia.setValue(datos.colonia);
+            this.direccion.municipio.setValue(datos.municipio);
+            this.direccion.estado.setValue(datos.estado);
+            this.direccion.cp.setValue(datos.cp);
+            this.direccion.colonia.setValue(datos.colonia);
+            this.direccion.calle.setValue(datos.calle);
+
+            this.direccion.noInterior.setValue(datos.numExterior);
+            this.direccion.noExterior.setValue(datos.numInterior);
+            this.idDomicilio = datos.idDomicilio;
+
+            return;
+          }
+          this.limpiarConsultaDatosPersonales();
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      );
+  }
+
+  consultaCP(): void {
+    this.loaderService.activar();
+    if (!this.direccion.cp.value) {
+      return;
+    }
+    this.gestionarOrdenServicioService
+      .consutaCP(this.direccion.cp.value)
+      .pipe(finalize(() => this.loaderService.desactivar()))
+      .subscribe(
+        (respuesta: HttpRespuesta<any>) => {
+          if (respuesta) {
+            this.direccion.colonia.setValue(respuesta.datos[0].nombre);
+            this.direccion.municipio.setValue(
+              respuesta.datos[0].localidad.municipio.nombre
+            );
+            this.direccion.estado.setValue(
+              respuesta.datos[0].localidad.municipio.entidadFederativa.nombre
+            );
+            return;
+          }
+          this.direccion.colonia.patchValue(null);
+          this.direccion.municipio.patchValue(null);
+          this.direccion.estado.patchValue(null);
+        },
+        (error: HttpErrorResponse) => {
+          console.log(error);
+        }
+      );
+  }
+
+  noEspaciosAlPrincipio(posicion: number) {
+    const formName = [
+      this.datosContratante.nombre,
+      this.datosContratante.primerApellido,
+      this.datosContratante.segundoApellido,
+    ];
+    formName[posicion].setValue(formName[posicion].value.trimStart());
+  }
+
+  cambiarTipoSexo(): void {
+    if (this.datosContratante.sexo.value == 3) {
+      this.datosContratante.otroTipoSexo.enabled;
+      this.datosContratante.otroTipoSexo.setValidators(Validators.required);
+      return;
+    }
+    this.datosContratante.otroTipoSexo.disabled;
+    this.datosContratante.otroTipoSexo.clearValidators();
+    this.datosContratante.otroTipoSexo.setValue(null);
+  }
+
+  cambiarNacionalidad(): void {
+    if (this.datosContratante.nacionalidad.value == 1) {
+      this.datosContratante.paisNacimiento.disabled;
+      this.datosContratante.paisNacimiento.clearValidators();
+      this.datosContratante.paisNacimiento.reset();
+      this.datosContratante.lugarNacimiento.enabled;
+      this.datosContratante.lugarNacimiento.setValidators(Validators.required);
+      return;
+    }
+    this.datosContratante.lugarNacimiento.disabled;
+    this.datosContratante.lugarNacimiento.clearValidators();
+    this.datosContratante.lugarNacimiento.reset();
+    this.datosContratante.paisNacimiento.enabled;
+    this.datosContratante.paisNacimiento.setValidators(Validators.required);
+  }
+
+  continuar() {}
   get datosContratante() {
     return (this.form.controls['datosContratante'] as FormGroup).controls;
   }
