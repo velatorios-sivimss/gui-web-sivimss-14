8ba0752214144c5ab4f72ffa4ac32ba003b62062
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-07-13 14:40:15 -0600

    CSS Opciones de Modificación de Pago

--- projects/sivimss-gui/src/app/modules/pagos/gestionar-pago/components/detalle-gestion-pago/detalle-gestion-pago.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/gestionar-pago/components/detalle-gestion-pago/detalle-gestion-pago.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {PagoDetalleGestion} from "../../models/pagoDetalleGestion.interface";
 import {ActivatedRoute, Router} from "@angular/router";
+import {OverlayPanel} from "primeng/overlaypanel";
+import {MetodoPagoGestion} from "../../models/metodoPagoGestion.interface";
 
 @Component({
   selector: 'app-detalle-gestion-pago',
@@ -9,12 +11,23 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class DetalleGestionPagoComponent implements OnInit {
 
+  @ViewChild(OverlayPanel)
+  overlayPanel!: OverlayPanel;
+
   registroPago!: PagoDetalleGestion;
+  pagoSeleccionado!: MetodoPagoGestion;
 
   constructor(
     private router: Router,
     private readonly activatedRoute: ActivatedRoute,
   ) {
+  }
+
+  mostrarOverlay(event: MouseEvent, pago: MetodoPagoGestion): void {
+    event.stopPropagation();
+    this.overlayPanel.toggle(event);
+    this.pagoSeleccionado = pago;
+    console.log(this.pagoSeleccionado);
   }
 
   ngOnInit(): void {
