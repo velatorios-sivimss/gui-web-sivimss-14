cc48e0fcae3afd41e2d6c3fe0dcd170cb19da025
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-07-18 14:42:14 -0600

    Eliminar Metodo de Pago

---
+++ projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/eliminar-tipo-pago/eliminar-tipo-pago.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eliminar-tipo-pago',
+  templateUrl: './eliminar-tipo-pago.component.html',
+  styleUrls: ['./eliminar-tipo-pago.component.scss']
+})
+export class EliminarTipoPagoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
