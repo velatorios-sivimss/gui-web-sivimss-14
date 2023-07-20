edb5d37be1e8df19039fc722ab83f76bae627e4f
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-07-20 07:52:59 -0600

    Ir a Pago Gestión

---
+++ projects/sivimss-gui/src/app/modules/pagos/gestionar-pago/components/ir-a-pago/ir-a-pago.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ir-a-pago',
+  templateUrl: './ir-a-pago.component.html',
+  styleUrls: ['./ir-a-pago.component.scss']
+})
+export class IrAPagoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
