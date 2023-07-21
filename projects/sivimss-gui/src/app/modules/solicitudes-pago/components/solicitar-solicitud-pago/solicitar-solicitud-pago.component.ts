5f3a07943217051b5157f0b81cbaf710d5f34c70
mikanyel <mikanyel@gmail.com>
2023-07-21 10:36:48 -0600

    ajustes en formularios para captura de informacion de acuerdo a tipo de solicitud

--- projects/sivimss-gui/src/app/modules/solicitudes-pago/components/solicitar-solicitud-pago/solicitar-solicitud-pago.component.ts
+++ projects/sivimss-gui/src/app/modules/solicitudes-pago/components/solicitar-solicitud-pago/solicitar-solicitud-pago.component.ts
@@ -6,7 +6,7 @@ import { TipoDropdown } from '../../../../models/tipo-dropdown';
 import {finalize} from "rxjs/operators";
 import { LoaderService } from 'projects/sivimss-gui/src/app/shared/loader/services/loader.service';
 import { SolicitudesPagoService } from '../../services/solicitudes-pago.service';
-import { SolicitarSolicitudPago, PartidaPresupuestal, CrearSolicitudPago } from '../../models/solicitud-pagos.interface';
+import { PartidaPresupuestal, CrearSolicitudPago } from '../../models/solicitud-pagos.interface';
 import {mapearArregloTipoDropdown} from 'projects/sivimss-gui/src/app/utils/funciones';
 import { MensajesSistemaService } from 'projects/sivimss-gui/src/app/services/mensajes-sistema.service';
 import { AlertaService , TipoAlerta} from '../../../../shared/alerta/services/alerta.service';
@@ -20,6 +20,7 @@ import * as moment from "moment/moment";
 })
 export class SolicitarSolicitudPagoComponent implements OnInit {
 
+  tipoSolicitudForm!: FormGroup;
   solicitarPagoForm1!: FormGroup;
   solicitarPagoForm2!: FormGroup;
   solicitarPagoForm3!: FormGroup;
@@ -32,7 +33,7 @@ export class SolicitarSolicitudPagoComponent implements OnInit {
   opcion5: boolean = false;
   ShowUnidadOpe: boolean = false;
   ShowUnidadAdmi: boolean = false;
-  nuevoSolicitudPago!: CrearSolicitudPago;
+  datosSolicitudPago!: CrearSolicitudPago;
   
 
   fechaActual: Date = new Date();
@@ -56,6 +57,7 @@ export class SolicitarSolicitudPagoComponent implements OnInit {
 
   ngOnInit(): void {
     debugger
+    this.inicializarTipoSolicitud();
     this.inicializarSolicitarPagoForm1();
     const respuesta = this.route.snapshot.data["respuesta"];
     this.catatalogoTipoSolicitud =  mapearArregloTipoDropdown(respuesta[this.POSICION_CATALOGO_TIPOSOLICITUD].datos, "desTipoSolicitud", "tipoSolicitud");
@@ -75,12 +77,15 @@ export class SolicitarSolicitudPagoComponent implements OnInit {
     ];
   }
 
+  inicializarTipoSolicitud(): void {
+    this.tipoSolicitudForm = this.formBulder.group({
+      tipoSolicitud: [{ value: null, disabled: false }, [ Validators.required]],
+    });
+  }
+
   inicializarSolicitarPagoForm1(): void {
     this.solicitarPagoForm1 = this.formBulder.group({
-      tipoSolicitud: [{ value: null, disabled: false }, [ Validators.required]],
-      unidadOpe: [{value:null, disabled: false}],
-      unidadAdmi: [{value:null, disabled: false}],
-      fechaElaboracion1: [{ value: null, disabled: false }, [ Validators.required]],
+      fechaElaboracion1: [{value:null, disabled: false}],
       nomDestinatario1: [{ value: null, disabled: false }, [ Validators.required]],
       nomRemitente1: [{ value: null, disabled: false }, [ Validators.required]],
       referenciaTD1: [{ value: null, disabled: false }, [ Validators.required]],
@@ -92,23 +97,59 @@ export class SolicitarSolicitudPagoComponent implements OnInit {
     });
   }
 
-  
-  inicializarSolicitarPagoForm2(): void {
+  inicializarSolicitarPagoForm234(): void {
     this.solicitarPagoForm2 = this.formBulder.group({
-      tipoSolicitud: [{ value: null, disabled: false }, [ Validators.required]],
-      unidadOpe: [{value:null, disabled: false}],
-      unidadAdmi: [{value:null, disabled: false}],
-      fechaElaboracion1: [{ value: null, disabled: false }, [ Validators.required]],
-      nomDestinatario1: [{ value: null, disabled: false }, [ Validators.required]],
-      nomRemitente1: [{ value: null, disabled: false }, [ Validators.required]],
-      referenciaTD1: [{ value: null, disabled: false }, [ Validators.required]],
-      beneficiario1: [{ value: null, disabled: false }, [ Validators.required]],
-      concepto1: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(60)]],
-      importeTotal1: [{ value: null, disabled: false }, [ Validators.required]],
-      cantidad1: [{ value: null, disabled: false }, [ Validators.required]],
-      observ1: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(100)]],
+      folioGastos2: [{ value: null, disabled: false }, [ Validators.required]],
+      fechaElaboracion2: [{value:null, disabled: false}],
+      unidadOpe2: [{value:null, disabled: false}],
+      unidadAdmi2: [{ value: null, disabled: false }, [ Validators.required]],
+      refeUnidadOpe2: [{ value: null, disabled: false }, [ Validators.required]],
+      solicitadoOpePor2: [{ value: null, disabled: false }, [ Validators.required]],
+      refeUnidadAdmi2: [{ value: null, disabled: false }, [ Validators.required]],
+      solicitadoAdmiPor2: [{ value: null, disabled: false }, [ Validators.required]],
+      nombreDestinatario2: [{ value: null, disabled: false }, [ Validators.required]],
+      nomRemitente2: [{ value: null, disabled: false }, [ Validators.required]],
+      referenciaTD2: [{ value: null, disabled: false }, [ Validators.required]],
+      beneficiario2: [{ value: null, disabled: false }, [ Validators.required]],
+      concepto2: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(60)]],
+      cantidadLetra2: [{ value: null, disabled: false }, [ Validators.required]],
+      observ2: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(100)]],
     });
   }
+
+  inicializarSolicitarPagoForm5(): void {
+    this.solicitarPagoForm3 = this.formBulder.group({
+      folioGastos3: [{value:null, disabled: false}],
+      folioConsig3: [{value:null, disabled: false}],
+      unidadOpe3: [{ value: null, disabled: false }, [ Validators.required]],
+      fechaElaboracion3: [{ value: null, disabled: false }, [ Validators.required]],
+      nombreDestinatario3: [{ value: null, disabled: false }, [ Validators.required]],
+      nomRemitente3: [{ value: null, disabled: false }, [ Validators.required]],
+      referenciaTD3: [{ value: null, disabled: false }, [ Validators.required]],
+      beneficiario3: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(60)]],
+      concepto3: [{ value: null, disabled: false }, [ Validators.required]],
+      cantidadLetra3: [{ value: null, disabled: false }, [ Validators.required]],
+      observ3: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(100)]],
+    });
+  }
+
+  inicializarSolicitarPagoForm6(): void {
+    this.solicitarPagoForm4 = this.formBulder.group({
+      folioGastos4: [{value:null, disabled: false}],
+      folioConsig4: [{value:null, disabled: false}],
+      fechaElaboracion4: [{ value: null, disabled: false }, [ Validators.required]],
+      nombreDestinatario4: [{ value: null, disabled: false }, [ Validators.required]],
+      nomRemitente4: [{ value: null, disabled: false }, [ Validators.required]],
+      referenciaTD4: [{ value: null, disabled: false }, [ Validators.required]],
+      beneficiario4: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(60)]],
+      concepto4: [{ value: null, disabled: false }, [ Validators.required]],
+      cantidadLetra4: [{ value: null, disabled: false }, [ Validators.required]],
+      observ4: [{ value: null, disabled: false }, [Validators.required, Validators.maxLength(100)]],
+    });
+  }
+
+  
+
 
   solicitarPago(): void {
     this.referencia.close(false);
@@ -120,7 +161,7 @@ export class SolicitarSolicitudPagoComponent implements OnInit {
 
   validaTipoSolicitud(): void {
     debugger
-    const idTipo = this.solicitarPagoForm1.get('tipoSolicitud')?.value;
+    const idTipo = this.tipoSolicitudForm.get('tipoSolicitud')?.value;
 
     if (idTipo === 1) {
       this.opcion1 = true;
@@ -148,32 +189,123 @@ export class SolicitarSolicitudPagoComponent implements OnInit {
     }
   }
 
-  generarSolicitudPago(): CrearSolicitudPago {
+  generarSolicitudPago1(): CrearSolicitudPago {
     return {
-      idTipoSolic: this.solicitarPagoForm1.get("tipoSolicitud")?.value,
-      cveFolioGastos: this.solicitarPagoForm1.get("folioGastos")?.value,
-      cveFolioConsignados: this.solicitarPagoForm1.get("folioConsig")?.value,
-      idUnidadMedica: this.solicitarPagoForm1.get("unidadOpe3")?.value,
+      idTipoSolic: this.tipoSolicitudForm.get('tipoSolicitud')?.value,
+      cveFolioGastos: "null",
+      cveFolioConsignados: "null",
+      fechaElaboracion: "null",
+      idUnidadMedica:1,
       idDelegacion: 1, 
-      nomDestinatario: this.solicitarPagoForm1.get("nombreDestinatario3")?.value,
-      nomRemitente: this.solicitarPagoForm1.get("nomRemitente3")?.value,
-      numReferencia: this.solicitarPagoForm1.get("referenciaTD3")?.value,
-      idContratBenef: this.solicitarPagoForm1.get("beneficiario3")?.value,
-      fechaInicial: "10/07/2023",
-      fechaFinal: "12/07/2023",
-      concepto: this.solicitarPagoForm1.get("concepto3")?.value,
-      observaciones: this.solicitarPagoForm1.get("observ3")?.value,
+      nomDestinatario: this.solicitarPagoForm1.get("nomDestinatario1")?.value,
+      nomRemitente: this.solicitarPagoForm1.get("nomRemitente1")?.value,
+      numReferencia: this.solicitarPagoForm1.get("referenciaTD1")?.value,
+      idContratBenef: 1,
+      fechaInicial: "null",
+      fechaFinal: "null",
+      beneficiario: this.solicitarPagoForm1.get("beneficiario1")?.value,
+      concepto: this.solicitarPagoForm1.get("concepto1")?.value,
+      observaciones: this.solicitarPagoForm1.get("observ1")?.value,
       idVelatorio: 1,
       ejercicioFiscal:2022,
       idEstatusSol: 1
     };
   }
 
+  generarSolicitudPago234(): CrearSolicitudPago {
+    return {
+      idTipoSolic: this.tipoSolicitudForm.get('tipoSolicitud')?.value,
+      cveFolioGastos: this.solicitarPagoForm2.get("folioGastos2")?.value,
+      fechaElaboracion: this.solicitarPagoForm2.get("fechaElaboracion2")?.value,
+      cveFolioConsignados: "null",
+      idUnidadMedica: this.solicitarPagoForm2.get("unidadOpe2")?.value,
+      idDelegacion: 1, 
+      nomDestinatario: this.solicitarPagoForm2.get("nombreDestinatario2")?.value,
+      nomRemitente: this.solicitarPagoForm2.get("nomRemitente2")?.value,
+      numReferencia: this.solicitarPagoForm2.get("referenciaTD2")?.value,
+      idContratBenef: 1,
+      fechaInicial: "null",
+      fechaFinal: "null",
+      beneficiario: this.solicitarPagoForm2.get("beneficiario2")?.value,
+      concepto: this.solicitarPagoForm2.get("concepto2")?.value,
+      observaciones: this.solicitarPagoForm2.get("observ2")?.value,
+      idVelatorio: 1,
+      ejercicioFiscal:2022,
+      idEstatusSol: 1
+    };
+  }
+
+  generarSolicitudPago5(): CrearSolicitudPago {
+    return {
+      idTipoSolic: this.tipoSolicitudForm.get('tipoSolicitud')?.value,
+      cveFolioGastos: this.solicitarPagoForm3.get("folioGastos3")?.value,
+      cveFolioConsignados: this.solicitarPagoForm3.get("folioConsig3")?.value,
+      fechaElaboracion: this.solicitarPagoForm3.get("fechaElaboracion3")?.value,
+      idUnidadMedica: 1,
+      idDelegacion: 1, 
+      nomDestinatario: this.solicitarPagoForm3.get("nombreDestinatario3")?.value,
+      nomRemitente: this.solicitarPagoForm3.get("nomRemitente3")?.value,
+      numReferencia: this.solicitarPagoForm3.get("referenciaTD3")?.value,
+      idContratBenef: this.solicitarPagoForm3.get("beneficiario3")?.value,
+      fechaInicial: "10/07/2023",
+      fechaFinal: "12/07/2023",
+      beneficiario: "null",
+      concepto: this.solicitarPagoForm3.get("concepto3")?.value,
+      observaciones: this.solicitarPagoForm3.get("observ3")?.value,
+      idVelatorio: 1,
+      ejercicioFiscal:2022,
+      idEstatusSol: 1
+    };
+  }
+
+  generarSolicitudPago6(): CrearSolicitudPago {
+    return {
+      idTipoSolic: this.tipoSolicitudForm.get('tipoSolicitud')?.value,
+      cveFolioGastos: this.solicitarPagoForm4.get("folioGastos4")?.value,
+      cveFolioConsignados: this.solicitarPagoForm4.get("folioConsig4")?.value,
+      fechaElaboracion: this.solicitarPagoForm4.get("fechaElaboracion4")?.value,
+      idUnidadMedica: 1,
+      idDelegacion: 1, 
+      nomDestinatario: this.solicitarPagoForm4.get("nombreDestinatario4")?.value,
+      nomRemitente: this.solicitarPagoForm4.get("nomRemitente4")?.value,
+      numReferencia: this.solicitarPagoForm4.get("referenciaTD4")?.value,
+      idContratBenef: this.solicitarPagoForm4.get("beneficiario4")?.value,
+      fechaInicial: "10/07/2023",
+      fechaFinal: "12/07/2023",
+      beneficiario: "null",
+      concepto: this.solicitarPagoForm4.get("concepto4")?.value,
+      observaciones: this.solicitarPagoForm4.get("observ4")?.value,
+      idVelatorio: 1,
+      ejercicioFiscal:2022,
+      idEstatusSol: 1
+    };
+  }
   
   crearSolicitudPago(): void {
     this.cargadorService.activar();
-    const solicitudGuardar: CrearSolicitudPago = this.generarSolicitudPago();
-    this.solicitudesPagoService.guardar(solicitudGuardar).pipe(
+    const idTipo = this.tipoSolicitudForm.get('tipoSolicitud')?.value;
+
+    switch (idTipo) {
+      case 1:
+        this.datosSolicitudPago = this.generarSolicitudPago1();
+      break;
+      case 2:
+        this.datosSolicitudPago = this.generarSolicitudPago234();
+      break;
+      case 3:
+        this.datosSolicitudPago = this.generarSolicitudPago234();
+      break;
+      case 4:
+        this.datosSolicitudPago = this.generarSolicitudPago234();
+      break;
+      case 5:
+        this.datosSolicitudPago = this.generarSolicitudPago5();
+      break;
+      case 6:
+        this.datosSolicitudPago = this.generarSolicitudPago6();
+      break;
+    }
+    this.solicitudesPagoService.guardar(this.datosSolicitudPago).pipe(
       finalize(() => this.cargadorService.desactivar())
     ).subscribe({
       next: (): void => {
@@ -206,6 +338,9 @@ export class SolicitarSolicitudPagoComponent implements OnInit {
       this.ShowUnidadAdmi = false;
       this.ShowUnidadOpe = true;
 
+  }
+
+  ngAfterViewInit() {
   }
 
 }
