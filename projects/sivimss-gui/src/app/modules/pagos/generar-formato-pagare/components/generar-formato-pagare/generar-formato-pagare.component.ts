e75196162888a9b9cb0dc0596e871bbd2ecba9ce
mikanyel <mikanyel@gmail.com>
2023-08-03 13:04:20 -0600

    se ajusta la busqueda por filtros nombre del contratante

--- projects/sivimss-gui/src/app/modules/pagos/generar-formato-pagare/components/generar-formato-pagare/generar-formato-pagare.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/generar-formato-pagare/components/generar-formato-pagare/generar-formato-pagare.component.ts
@@ -186,13 +186,13 @@ export class GenerarFormatoPagareComponent implements OnInit {
     const fechaInicial = this.filtroForm.get('fechaInicial')?.value !== null ? moment(this.filtroForm.get('fechaInicial')?.value).format('DD/MM/YYYY') : null;
     const fechaFinal = this.filtroForm.get('fechaFinal')?.value !== null ? moment(this.filtroForm.get('fechaFinal')?.value).format('DD/MM/YYYY') : null;
     const folio = this.filtroForm.get("folioODS")?.value !== null ? this.filtroForm.get("folioODS")?.value.label : null;
+    const nomContratante = this.filtroForm.get("nombreContratante")?.value !== null ? this.filtroForm.get("nombreContratante")?.value.label : null;
     return {
-      idOficina: this.filtroForm.get("oficina")?.value,
       idNivel: this.filtroForm.get("nivel")?.value,
       idDelegacion: this.filtroForm.get("delegacion")?.value,
       idVelatorio: this.filtroForm.get("velatorio")?.value,
       folioODS: folio,
-      nomContratante: this.filtroForm.get("nombreContratante")?.value,
+      nomContratante: nomContratante,
       fecIniODS: fechaInicial,
       fecFinODS: fechaFinal,
       tipoReporte: tipoReporte
@@ -300,7 +300,6 @@ export class GenerarFormatoPagareComponent implements OnInit {
       },
     })
   }
-  ngAfterViewInit() {
-  }
+
 
 }
--- projects/sivimss-gui/src/app/modules/pagos/generar-formato-pagare/models/filtrosFormatoPagare.interface.ts
+++ projects/sivimss-gui/src/app/modules/pagos/generar-formato-pagare/models/filtrosFormatoPagare.interface.ts
@@ -1,5 +1,4 @@
 export interface FiltrosFormatoPagare {
-  "idOficina": number,
   "idNivel": number,
   "idDelegacion": number,
   "idVelatorio": number,
