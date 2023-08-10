cbb8e71da82df7f590d276124014e9f787a23983
Antonio Alcantar <antonio.alcantar@people-media.com.mx>
2023-08-09 18:36:26 -0600

    Se ajusta valor del monto total calculado de todos los registros. Se ajusta mapeo del valor de la columna mantenimiento.

--- projects/sivimss-gui/src/app/modules/mantenimiento-vehicular/components/reporte-encargado/reporte-encargado.component.ts
+++ projects/sivimss-gui/src/app/modules/mantenimiento-vehicular/components/reporte-encargado/reporte-encargado.component.ts
@@ -149,7 +149,7 @@ export class ReporteEncargadoComponent implements OnInit {
             this.totalVehiculos = respuesta.datos.totalElements;
             this.totalElementos = respuesta.datos.totalElements;
             this.registrosReporte = respuesta.datos.content;
-            this.sumTotal = respuesta.datos.content.reduce((sum: any, val: any) => (sum + parseFloat(val.MON_COSTO_MTTO ? val.MON_COSTO_MTTO : 0)), 0);
+            this.sumTotal = respuesta.datos.content[0].totalCosto;
           },
           error: (error: HttpErrorResponse): void => {
             console.error(error);
@@ -163,7 +163,7 @@ export class ReporteEncargadoComponent implements OnInit {
             this.totalVehiculos = respuesta.datos.totalElements;
             this.totalElementos = respuesta.datos.totalElements;
             this.registrosReporte = respuesta.datos.content;
-            this.sumTotal = respuesta.datos.content.reduce((sum: any, val: any) => (sum + parseFloat(val.MON_COSTO_MTTO ? val.MON_COSTO_MTTO : 0)), 0);
+            this.sumTotal = respuesta.datos.content[0].totalCosto;
           },
           error: (error: HttpErrorResponse): void => {
             console.error(error);
