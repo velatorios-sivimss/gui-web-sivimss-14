a9527bf6330112ea0c9a4c99b97d71c85f933a57
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-08-15 17:12:25 -0600

    Implementación de Método tipo de solicitud vale

--- projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/registrar-pago/registrar-vale-paritaria/registrar-vale-paritaria.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/registrar-pago/registrar-vale-paritaria/registrar-vale-paritaria.component.ts
@@ -1,6 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {DynamicDialogConfig, DynamicDialogRef} from "primeng/dynamicdialog";
+import {SolicitudCrearPago} from "../../../modelos/solicitudPago.interface";
+import {AlertaService, TipoAlerta} from "../../../../../../shared/alerta/services/alerta.service";
+import {HttpErrorResponse} from "@angular/common/http";
+import {RealizarPagoService} from "../../../services/realizar-pago.service";
+import {MensajesSistemaService} from "../../../../../../services/mensajes-sistema.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import * as moment from "moment";
 
 interface DatosRegistro {
   idPagoBitacora: number,
@@ -28,7 +35,13 @@ export class RegistrarValeParitariaComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
               public config: DynamicDialogConfig,
-              public ref: DynamicDialogRef,) {
+              public ref: DynamicDialogRef,
+              private realizarPagoService: RealizarPagoService,
+              private alertaService: AlertaService,
+              private mensajesSistemaService: MensajesSistemaService,
+              private router: Router,
+              private readonly activatedRoute: ActivatedRoute
+  ) {
   }
 
   ngOnInit(): void {
@@ -41,7 +54,7 @@ export class RegistrarValeParitariaComponent implements OnInit {
     this.valeParitariaForm = this.formBuilder.group({
       numAutorizacion: [{value: null, disabled: false}, [Validators.required]],
       fechaValeAGF: [{value: null, disabled: false}, [Validators.required]],
-      importePago: [{value: null, disabled: false}, [Validators.required]],
+      importe: [{value: null, disabled: false}, [Validators.required]],
     });
   }
 
@@ -55,7 +68,36 @@ export class RegistrarValeParitariaComponent implements OnInit {
   }
 
   guardar(): void {
+    const solicitudPago: SolicitudCrearPago = this.generarSolicitudPago();
+    this.realizarPagoService.guardar(solicitudPago).subscribe({
+      next: (): void => {
+        this.alertaService.mostrar(TipoAlerta.Exito, 'Pago registrado correctamente');
+        this.ref.close();
+        void this.router.navigate(["../"], {relativeTo: this.activatedRoute});
+      },
+      error: (error: HttpErrorResponse): void => {
+        const ERROR: string = 'Error al guardar la información del Pago. Intenta nuevamente.'
+        this.mensajesSistemaService.mostrarMensajeError(error, ERROR);
+        console.log(error);
+      }
+    });
+  }
 
+  generarSolicitudPago(): SolicitudCrearPago {
+    let fechaValeAGF = this.valeParitariaForm.get('fechaValeAGF')?.value;
+    if (fechaValeAGF) fechaValeAGF = moment(fechaValeAGF).format('YYYY-MM-DD');
+    return {
+      descBanco: '',
+      fechaPago: null,
+      fechaValeAGF: fechaValeAGF,
+      idFlujoPago: this.registroPago.idFlujoPago,
+      idMetodoPago: 1,
+      idPagoBitacora: this.registroPago.idPagoBitacora,
+      idRegistro: this.registroPago.idRegistro,
+      importePago: this.valeParitariaForm.get('importe')?.value,
+      importeRegistro: this.registroPago.importePago,
+      numAutorizacion: this.valeParitariaForm.get('noAutorizacion')?.value
+    }
   }
 
   get pf() {
