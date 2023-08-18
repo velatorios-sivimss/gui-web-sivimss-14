d48fecf8217c63567ec5ca11caf37dc0838230cd
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-08-18 13:35:09 -0600

    Primera Parte Componente Datos de Contratante

--- projects/sivimss-gui/src/app/modules/pagos/facturacion/components/datos-contratante/datos-contratante.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/facturacion/components/datos-contratante/datos-contratante.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-datos-contratante',
   templateUrl: './datos-contratante.component.html',
   styleUrls: ['./datos-contratante.component.scss']
 })
-export class DatosContratanteComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class DatosContratanteComponent {
+  @Input() nombreContratante: string = '';
+  @Input() fecha: string = '';
+  @Input() tipoFecha: 1 | 2 | 3 | 4 = 1;
+  @Input() fechaPago: string = '';
+  @Input() concepto: string = '';
 }
