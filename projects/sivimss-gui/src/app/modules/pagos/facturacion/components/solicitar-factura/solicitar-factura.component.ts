8210ea57075e53e36617ec4ee229c911af8bcfd8
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-08-18 12:19:57 -0600

    Filtros de Tipo de Solicitud

--- projects/sivimss-gui/src/app/modules/pagos/facturacion/components/solicitar-factura/solicitar-factura.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/facturacion/components/solicitar-factura/solicitar-factura.component.ts
@@ -1,8 +1,9 @@
-import {Component, OnInit} from '@angular/core';
-import {FormGroup} from "@angular/forms";
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormBuilder, FormGroup, FormGroupDirective, Validators} from "@angular/forms";
 import {TipoDropdown} from "../../../../../models/tipo-dropdown";
 import {DIEZ_ELEMENTOS_POR_PAGINA} from "../../../../../utils/constantes";
 import {REGISTROS_PAGOS} from "../../constants/dummies";
+import {TIPO_FACTURACION} from "../../constants/tipoFacturacion";
 
 @Component({
   selector: 'app-solicitar-factura',
@@ -11,19 +12,47 @@ import {REGISTROS_PAGOS} from "../../constants/dummies";
 })
 export class SolicitarFacturaComponent implements OnInit {
 
+  @ViewChild(FormGroupDirective)
+  private filtroFormDir!: FormGroupDirective;
 
   solicitudForm!: FormGroup;
   indice: number = 0;
-  tiposFactura: TipoDropdown[] = [];
+  tiposFactura: TipoDropdown[] = TIPO_FACTURACION;
   folios: TipoDropdown[] = [];
   datosContratanteForm!: FormGroup;
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
   ngOnInit(): void {
+    this.inicializarForm();
+  }
+
+  inicializarForm(): void {
+    this.solicitudForm = this.formBuilder.group({
+      tipoFactura: [{value: null, disabled: false}, [Validators.required]],
+      folio: [{value: null, disabled: false}, [Validators.required]],
+    });
+  }
+
+  limpiar(): void {
+    if (this.solicitudForm) {
+      this.filtroFormDir.resetForm({});
+    }
   }
 
   protected readonly DIEZ_ELEMENTOS_POR_PAGINA = DIEZ_ELEMENTOS_POR_PAGINA;
   servicios: any[] = REGISTROS_PAGOS;
+
+  get folio(): string {
+    const tipoFactura = this.solicitudForm.get('tipoFactura')?.value;
+    if (![1, 2, 3, 4].includes(tipoFactura)) return '';
+    if (tipoFactura === 1) return 'Folio de ODS';
+    if (tipoFactura === 3) return 'Número de permiso';
+    return 'Folio del Convenio';
+  }
+
+  get pf() {
+    return this.solicitudForm?.controls;
+  }
 }
