cc1e8eb5c41f5df2bb932f3f039edbf3827c47f8
mikanyel <mikanyel@gmail.com>
2023-08-18 20:36:54 -0600

    Se integra la funcionalidad para descargar el reporte del detalle de comisiones

--- projects/sivimss-gui/src/app/modules/calculo-comisiones/models/comisiones.interface.ts
+++ projects/sivimss-gui/src/app/modules/calculo-comisiones/models/comisiones.interface.ts
@@ -1,7 +1,7 @@
 export interface Comisiones {
   idPromotor: number;
   numEmpleado: string;
-  curp: string,
+  curp: string;
   nombre: string;
   primerApellido: string;
   segundoApellido: string;
