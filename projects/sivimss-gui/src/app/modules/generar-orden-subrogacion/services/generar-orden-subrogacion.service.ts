23e87b94aaae08ae9b5659d1b132fca1c03beebd
Antonio Alcantar <antonio.alcantar@people-media.com.mx>
2023-09-11 15:47:09 -0600

    Se suben ajustes de integración del CU37

--- projects/sivimss-gui/src/app/modules/generar-orden-subrogacion/services/generar-orden-subrogacion.service.ts
+++ projects/sivimss-gui/src/app/modules/generar-orden-subrogacion/services/generar-orden-subrogacion.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
 import { Injectable } from '@angular/core';
 import { environment } from 'projects/sivimss-gui/src/environments/environment';
 import { Observable, of } from 'rxjs';
@@ -11,9 +11,17 @@ import { mapearArregloTipoDropdown } from '../../../utils/funciones';
 
 @Injectable()
 export class GenerarOrdenSubrogacionService extends BaseService<HttpRespuesta<any>, any> {
+
+  ordenSeleccionada: any;
+
   constructor(_http: HttpClient, private authService: AutenticacionService) {
     super(_http, `${environment.api.mssivimss}`, "agregar-order", "actualizar-orden",
       37, "orden", "detalle-orden", "cambiar-estatus");
+  }
+
+  obtenerVelatorios(delegacion: string | null = null): Observable<HttpRespuesta<any>> {
+    const body: any = { idDelegacion: delegacion }
+    return this._http.post<HttpRespuesta<any>>(`${environment.api.login}/velatorio/consulta`, body);
   }
 
   obtenerCatalogoNiveles(): Observable<TipoDropdown[]> {
@@ -30,4 +38,58 @@ export class GenerarOrdenSubrogacionService extends BaseService<HttpRespuesta<an
     const body = { idDelegacion: delegacion }
     return this._http.post<HttpRespuesta<any>>(`${environment.api.login}/velatorio/consulta`, body);
   }
+
+  consultarFolios(folio: string, idVelatorio: string): Observable<HttpRespuesta<any>> {
+    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/consultar-folio-orden`,
+      { idVelatorio: idVelatorio, folioOrdenServicio: folio });
+  }
+
+  consultarProveedor(idVelatorio: string, proveedor: string): Observable<HttpRespuesta<any>> {
+    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/consultar-proveedor`,
+      { idVelatorio: idVelatorio, nombreProveedor: proveedor });
+  }
+
+  consultarServicios(idOrdenServicio: number): Observable<HttpRespuesta<any>> {
+    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/buscar-filtros/hoja-subrogacion-servicios`,
+      { idOrdenServicio: idOrdenServicio });
+  }
+
+  buscarPorFiltros(pagina: number, tamanio: number, t: any): Observable<HttpRespuesta<any>> {
+    const params: HttpParams = new HttpParams()
+      .append("pagina", pagina)
+      .append("tamanio", tamanio);
+    return this._http.post<HttpRespuesta<any>>(`${this._base}${this._funcionalidad}/buscar/hoja-subrogacion-filtros`, t,
+      { params });
+  }
+
+  guardarOrden(orden: any): Observable<HttpRespuesta<any>> {
+    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/inserta-hoja-subrogacion`, orden);
+  }
+
+  actualizarOrden(orden: any): Observable<HttpRespuesta<any>> {
+    return this._http.post<HttpRespuesta<any>>(this._base + `${this._funcionalidad}/modifica-hoja-subrogacion`, orden);
+  }
+
+  // generarReporte(datosReporte: any): Observable<Blob> {
+  //   const headers = new HttpHeaders({
+  //     'Content-Type': 'application/json',
+  //     Accept: 'application/json'
+  //   });
+  //   return this._http.post<any>(this._base + `${this._funcionalidad}/buscar/reporte-hoja-subrogacion`
+  //     , datosReporte, { headers, responseType: 'blob' as 'json' });
+  // }
+
+  generarReporte(datosReporte:any) : Observable<HttpRespuesta<any>> {
+    return this._http.post<any>(this._base + `${this._funcionalidad}/buscar/reporte-hoja-subrogacion`, datosReporte)
+  }
+
+  generarHojaSubrogacion(datosReporte: any): Observable<Blob> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    });
+    return this._http.post<any>(this._base + `${this._funcionalidad}/hoja-subrogacion-generar-pdf/generarDocumento/pdf`
+      , datosReporte, { headers, responseType: 'blob' as 'json' });
+  }
+
 }
