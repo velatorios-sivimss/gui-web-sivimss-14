08ff80f8af3fbda8e9804a0d1019ee1b9b9f1d22
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-09-21 11:16:28 -0600

    Correcciones SONAR constructor y else-if

--- projects/sivimss-gui/src/app/modules/capillas/services/capillas.resolver.ts
+++ projects/sivimss-gui/src/app/modules/capillas/services/capillas.resolver.ts
@@ -1,45 +1,22 @@
-import { Capilla } from './../models/capilla.interface';
-// TODO: Regresar catalogos
-
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {
-    ActivatedRouteSnapshot, Resolve,
-    RouterStateSnapshot
+  ActivatedRouteSnapshot, Resolve,
+  RouterStateSnapshot
 } from '@angular/router';
-import { forkJoin, Observable, of } from 'rxjs';
+import {Observable} from 'rxjs';
 
-import { CapillaService } from './capilla.service';
-import { HttpRespuesta } from '../../../models/http-respuesta.interface';
-// import { CapillaService } from './articulos.service';
+import {CapillaService} from './capilla.service';
+import {HttpRespuesta} from '../../../models/http-respuesta.interface';
 
 
 @Injectable()
 export class CapillasResolver implements Resolve<HttpRespuesta<any>> {
 
-  constructor(private capillaService: CapillaService) { }
+  constructor(private capillaService: CapillaService) {
+  }
 
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<HttpRespuesta<any>> {
-    // utils catalogoCapillas$ = this.capillaService.obtenerCatalogoCapillas();
-    // utils catalogoTiposArticulos$ = this.capillaService.obtenerCatalogoTiposArticulos();
-    // utils catalogoTiposMateriales$ = this.capillaService.obtenerCatalogoTiposMateriales();
-    // utils catalogoTamanios$ = this.capillaService.obtenerCatalogoTamanios();
-    // utils catalogoClasificacionProductos$ = this.capillaService.obtenerCatalogoClasificacionProductos();
-    // utils catalogoPartidasPresupuestales$ = this.capillaService.obtenerCatalogoPartidasPresupuestales();
-    // utils catalogoCuentasContables$ = this.capillaService.obtenerCatalogoCuentasContables();
-    // utils catalogoClavesSat$ = this.capillaService.obtenerCatalogoClavesSat();
     return this.capillaService.obtenerCatalogoCapillas();
-    // return
-    //   catalogoCapillas$,
-        // catalogoTiposArticulos$,
-        // catalogoTiposMateriales$,
-        // catalogoTamanios$,
-        // catalogoClasificacionProductos$,
-        // catalogoPartidasPresupuestales$,
-        // catalogoCuentasContables$,
-        // catalogoClavesSat$,
-      //  return this.capillaService.obtenerCatalogoCapillas();
-      // return of();
-
   }
 }
