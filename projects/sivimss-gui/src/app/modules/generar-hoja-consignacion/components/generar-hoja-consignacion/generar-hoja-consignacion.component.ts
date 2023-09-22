090aaade3ec2caccdcd8e8635337345a3080560e
GRUPOPM\mparedes <martin.paredes@people-media.com.mx>
2023-09-22 09:32:18 -0600

    Ajustes CU039

--- projects/sivimss-gui/src/app/modules/generar-hoja-consignacion/components/generar-hoja-consignacion/generar-hoja-consignacion.component.ts
+++ projects/sivimss-gui/src/app/modules/generar-hoja-consignacion/components/generar-hoja-consignacion/generar-hoja-consignacion.component.ts
@@ -5,7 +5,7 @@ import { AlertaService, TipoAlerta } from "../../../../shared/alerta/services/al
 import { OverlayPanel } from "primeng/overlaypanel";
 import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
 import { DIEZ_ELEMENTOS_POR_PAGINA } from "../../../../utils/constantes";
-import { BuscarGenerarHojaConsignacion, GenerarHojaConsignacion, GenerarHojaConsignacionBusqueda } from "../../models/generar-hoja-consignacion.interface";
+import { BuscarGenerarHojaConsignacion, GenerarHojaConsignacionBusqueda } from "../../models/generar-hoja-consignacion.interface";
 import { LazyLoadEvent } from "primeng/api";
 import { ActivatedRoute, Router } from '@angular/router';
 import { GENERAR_FORMATO_BREADCRUMB } from '../../constants/breadcrumb';
@@ -23,11 +23,6 @@ import { finalize } from 'rxjs';
 import { AgregarGenerarHojaConsignacionComponent } from '../agregar-generar-hoja-consignacion/agregar-generar-hoja-consignacion.component';
 import * as moment from 'moment';
 import { FacturaProveedorComponent } from '../factura-proveedor/factura-proveedor.component';
-
-interface HttpResponse {
-  respuesta: string;
-  promotor: GenerarHojaConsignacion;
-}
 
 @Component({
   selector: 'app-generar-hoja-consignacion',
@@ -54,11 +49,11 @@ export class GenerarHojaConsignacionComponent implements OnInit {
   public mensajeArchivoConfirmacion: string = "";
 
   public hojasConsignacion: GenerarHojaConsignacionBusqueda[] = [
-    {
-      idHojaConsignacion: 1,
-      fecHojaConsignacion: '14/09/2023',
-      folioHojaConsignacion: '14/09/2023',
-    }
+    // {
+    //   idHojaConsignacion: 1,
+    //   fecHojaConsignacion: '14/09/2023',
+    //   folioHojaConsignacion: '14/09/2023',
+    // }
   ];
   public actividadSeleccionada!: GenerarHojaConsignacionBusqueda;
   public detalleRef!: DynamicDialogRef;
@@ -184,7 +179,7 @@ export class GenerarHojaConsignacionComponent implements OnInit {
           console.error(error);
           this.alertaService.mostrar(TipoAlerta.Error, error.message);
         }
-    });
+      });
   }
 
   datosPromotoresFiltros(): BuscarGenerarHojaConsignacion {
@@ -201,11 +196,22 @@ export class GenerarHojaConsignacionComponent implements OnInit {
   }
 
   agregarFormatoActividades(): void {
-    void this.router.navigate([`agregar-hoja`], { relativeTo: this.activatedRoute });
-  }
+    if (this.filtroForm.valid) {
 
-  modificarFormatoActividades(actividadSeleccionada: GenerarHojaConsignacionBusqueda): void {
-    void this.router.navigate([`modificar-hoja/${actividadSeleccionada.idHojaConsignacion}`], { relativeTo: this.activatedRoute });
+      this.generarHojaConsignacionService.delegacionSeleccionada =
+        this.catalogoDelegaciones.find((item: TipoDropdown) => item.value === this.ff.delegacion.value);
+
+      this.generarHojaConsignacionService.velatorioSeleccionado =
+        this.catalogoVelatorios.find((item: TipoDropdown) => item.value === this.ff.velatorio.value);
+
+      this.generarHojaConsignacionService.proveedorSeleccionado =
+        this.catalogoProveedores.find((item: TipoDropdown) => item.value === this.ff.proveedor.value);
+
+      this.generarHojaConsignacionService.fecInicio = moment(this.ff.fecInicio.value).format('DD/MM/YYYY');
+      this.generarHojaConsignacionService.fecFin = moment(this.ff.fecFin.value).format('DD/MM/YYYY');
+
+      void this.router.navigate([`agregar-hoja`], { relativeTo: this.activatedRoute });
+    }
   }
 
   detalleFormatoActividades(actividadSeleccionada: GenerarHojaConsignacionBusqueda): void {
@@ -221,7 +227,7 @@ export class GenerarHojaConsignacionComponent implements OnInit {
     this.detalleRef = this.dialogService.open(FacturaProveedorComponent, {
       header: "Factura proveedor",
       width: "920px",
-      data: {actividad: this.actividadSeleccionada},
+      data: { actividad: this.actividadSeleccionada },
     });
   }
 
--- projects/sivimss-gui/src/app/modules/generar-hoja-consignacion/models/generar-hoja-consignacion.interface.ts
+++ projects/sivimss-gui/src/app/modules/generar-hoja-consignacion/models/generar-hoja-consignacion.interface.ts
@@ -1,32 +1,53 @@
-export interface ProveedoresBusqueda {
-  proveedor?: string | null;
-  fecha?: string | null;
-  folio?: string | null;
-  categoria?: string | null;
-  folioArticulo?: string | null;
-  paquete?: string | null;
-  costo?: string | null;
-}
-export interface GenerarHojaConsignacion {
-  idFormatoRegistro?: number;
-  idVelatorio?: number;
-  fecInicio?: string;
-  fecFin?: string;
-  actividades?: Actividades;
+export interface ArticulosBusqueda {
+  totalCosto?: number | null;
+  totalArt?: number | null;
+  articulosBusquedaDetalle: ArticulosBusquedaDetalle[];
 }
 
-export interface Actividades {
-  idActividad?: number | null;
-  fecActividad?: string | null;
-  hrInicio?: string | null;
-  hrFin?: string | null;
-  idPromotor?: number | null;
-  numPlaticas?: number | null;
-  unidad?: string | null;
-  empresa?: string | null;
-  actividadRealizada?: string | null;
-  observaciones?: string | null;
-  evidencia?: boolean | null;
+export interface ArticulosBusquedaDetalle {
+  idArticulo?: number | null;
+  idOds?: number | null;
+  idPaquete?: number | null;
+  folioOds?: string | null;
+  fecOds?: string | null;
+  folioOde?: string | null;
+  costoUnitario?: number | null;
+  costoConIva?: number | null;
+  categoria?: string | null;
+  proveedor?: string | null;
+  paquete?: string | null;
+
+}
+export interface GenerarHoja {
+  idVelatorio?: any;
+  idProveedor?: any;
+  artConsig?: ArticulosConsignacion[];
+}
+
+export interface ArticulosConsignacion {
+  idArticulo?: number | null;
+  idOds?: number | null;
+  idPaquete?: number | null;
+  folioOds?: string | null;
+  fecOds?: string | null;
+  folioOde?: string | null;
+  costoUnitario?: number | null;
+  costoConIva?: number | null;
+  categoria?: string | null;
+  proveedor?: string | null;
+  paquete?: string | null;
+}
+
+export interface GenerarHojaConsignacion {
+
+}
+
+export interface BuscarProveedor {
+  idDelegacion?: any;
+  idVelatorio?: any;
+  idProveedor?: any;
+  fecInicio?: string | null;
+  fecFin?: string | null;
 }
 
 export interface BuscarGenerarHojaConsignacion {
@@ -49,4 +70,10 @@ export interface BuscarCatalogo {
   catalogo?: number | null;
   idCatalogo?: number | null;
   idVelatorio?: number | null;
+}
+
+export interface AdjuntarFactura {
+  idHojaConsig: number | null;
+  folioFiscal: string | null;
+  costoFactura: number | null;
 }
