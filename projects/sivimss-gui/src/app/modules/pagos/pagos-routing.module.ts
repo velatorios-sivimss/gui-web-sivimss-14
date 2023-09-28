ce694dc0cda4a562a40a44d5f1b67fe9c3de450a
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-09-28 11:01:49 -0600

    Se elimina modulo useless realizar balance de cajas

--- projects/sivimss-gui/src/app/modules/pagos/pagos-routing.module.ts
+++ projects/sivimss-gui/src/app/modules/pagos/pagos-routing.module.ts
@@ -1,7 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PermiteUsuarioLogueadoGuard } from "../../guards/permite-usuario-logueado.guard";
-import { PaginaNoEncontradaComponent } from "../../components/pagina-no-encontrada/pagina-no-encontrada.component";
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {PermiteUsuarioLogueadoGuard} from "../../guards/permite-usuario-logueado.guard";
+import {PaginaNoEncontradaComponent} from "../../components/pagina-no-encontrada/pagina-no-encontrada.component";
 
 const routes: Routes = [
   {
@@ -33,10 +33,6 @@ const routes: Routes = [
         path: 'gestionar-pago',
         loadChildren: () => import('./gestionar-pago/gestionar-pago.module').then(m => m.GestionarPagoModule),
       },
-      {
-        path: 'realizar-balance-caja',
-        loadChildren: () => import('./realizar-balance-caja/realizar-balance-caja.module').then(m => m.RealizarBalanceCajaModule)
-      }
     ]
   },
 
