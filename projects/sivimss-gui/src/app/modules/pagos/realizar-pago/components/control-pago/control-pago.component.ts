04b87aadc60ffea3a704d89a81cb4a7bb2b886b5
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-11-10 14:34:42 -0600

    CSS Diferentes cantidades

--- projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/control-pago/control-pago.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/control-pago/control-pago.component.ts
@@ -9,7 +9,8 @@ import {DialogService, DynamicDialogConfig} from "primeng/dynamicdialog";
 import {MAX_WIDTH} from "../../../../../utils/constantes";
 import {RegistrarTipoPagoComponent} from "../registrar-pago/registrar-tipo-pago/registrar-tipo-pago.component";
 import {OverlayPanel} from "primeng/overlaypanel";
-
+import {ModificarTipoPagoComponent} from "../modificar-tipo-pago/modificar-tipo-pago.component";
+import {EliminarTipoPagoComponent} from "../eliminar-tipo-pago/eliminar-tipo-pago.component";
 
 interface DatosRegistro {
   idPagoBitacora?: number,
@@ -25,6 +26,18 @@ interface RegistroModal {
   datosRegistro: DatosRegistro
 }
 
+interface ParametrosModificar {
+  metodoPago: string,
+  tipoPago: string,
+  importe: number,
+  idPagoDetalle: number
+}
+
+interface ParametrosCancelar {
+  pago: MetodoPago,
+  total: number
+}
+
 @Component({
   selector: 'app-control-pago',
   templateUrl: './control-pago.component.html',
@@ -37,6 +50,8 @@ export class ControlPagoComponent implements OnInit {
   overlayPanel!: OverlayPanel;
 
   registroPago!: DetallePago;
+  pagoSeleccionado!: MetodoPago;
+
   idPagoBitacora!: number;
   fecha: Date = new Date();
   tipoPago: string = '';
@@ -105,18 +120,6 @@ export class ControlPagoComponent implements OnInit {
     return TIPO_PAGO_CATALOGOS_CONVENIO;
   }
 
-  redireccionPago(): void {
-    if (this.registroPago.tipoPago === 'Pago de Orden de Servicio') {
-      void this.router.navigate(["./../../pago-orden-servicio"], {relativeTo: this.activatedRoute});
-      return;
-    }
-    if (this.registroPago.tipoPago === 'Pago de Nuevos Convenios de Previsión Funeraria') {
-      void this.router.navigate(["./../../pago-convenio-prevision-funeraria"], {relativeTo: this.activatedRoute});
-      return;
-    }
-    void this.router.navigate(["./../../pago-renovacion-convenio-prevision-funeraria"], {relativeTo: this.activatedRoute});
-  }
-
   registrarPago(): void {
     const idPago = this.pagoForm.get('tipoPago')?.value;
     const tipoPago: string = this.tiposPago.find((tp: TipoDropdown) => tp.value === idPago)?.label ?? '';
@@ -144,6 +147,34 @@ export class ControlPagoComponent implements OnInit {
 
   mostrarOverlay(event: MouseEvent, pago: MetodoPago): void {
     event.stopPropagation();
+    this.pagoSeleccionado = pago;
     this.overlayPanel.toggle(event);
   }
+
+  modificarTipoPago(): void {
+    const data: ParametrosModificar = {
+      metodoPago: this.pagoSeleccionado.metodoPago, importe: this.pagoSeleccionado.importe,
+      tipoPago: this.registroPago.tipoPago, idPagoDetalle: this.pagoSeleccionado.idPagoDetalle
+    };
+    const MODIFICAR_TIPO_PAGO_CONFIG: DynamicDialogConfig = {
+      header: "Modificar pago",
+      width: MAX_WIDTH,
+      data
+    };
+    this.dialogService.open(ModificarTipoPagoComponent, MODIFICAR_TIPO_PAGO_CONFIG)
+  }
+
+  cancelarTipoPago(): void {
+    const data: ParametrosCancelar = {
+      pago: this.pagoSeleccionado,
+      total: this.registroPago.totalAPagar
+    };
+    const CANCELAR_TIPO_PAGO_CONFIG: DynamicDialogConfig = {
+      header: "Cancelar pago",
+      width: MAX_WIDTH,
+      data
+    };
+    this.dialogService.open(EliminarTipoPagoComponent, CANCELAR_TIPO_PAGO_CONFIG);
+  }
+
 }
