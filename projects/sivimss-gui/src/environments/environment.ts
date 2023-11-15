3f5a794499a8d8ca308520ba24a7b4f1043c7d0e
Alberto Marin <alberto.marind@gmail.com>
2023-11-15 01:22:46 -0600

    Correccion btn-regresar

--- projects/sivimss-gui/src/environments/environment.ts
+++ projects/sivimss-gui/src/environments/environment.ts
@@ -5,17 +5,17 @@
 export const environment = {
   production: false,
   api: {
-    // mssivimss: 'mssivimss-ctrol-permisos/v1/sivimss/service/',
-    // login: 'mssivimss-oauth/v1',
-    // servicios_externos: 'mssivimss-ser-externos/v1/catalogos/externos/',
-    // servicios_externos_reportes: 'mssivimss-ser-externos/v1/reportes/',
-    // notificaciones: 'mssivimss-notificaciones/v1/notificaciones/avisos',
+    mssivimss: 'mssivimss-ctrol-permisos/v1/sivimss/service/',
+    login: 'mssivimss-oauth/v1',
+    servicios_externos: 'mssivimss-ser-externos/v1/catalogos/externos/',
+    servicios_externos_reportes: 'mssivimss-ser-externos/v1/reportes/',
+    notificaciones: 'mssivimss-notificaciones/v1/notificaciones/avisos',
 
-    mssivimss: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ctrol-permisos/v1/sivimss/service/',
-    login: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-oauth/v1',
-    servicios_externos: 'http://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ser-externos/v1/catalogos/externos/',
-    servicios_externos_reportes: 'http://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ser-externos/v1/reportes/',
-    notificaciones: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-notificaciones/v1/notificaciones/avisos',
+    // mssivimss: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ctrol-permisos/v1/sivimss/service/',
+    // login: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-oauth/v1',
+    // servicios_externos: 'http://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ser-externos/v1/catalogos/externos/',
+    // servicios_externos_reportes: 'http://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ser-externos/v1/reportes/',
+    // notificaciones: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-notificaciones/v1/notificaciones/avisos',
   },
 };
 
