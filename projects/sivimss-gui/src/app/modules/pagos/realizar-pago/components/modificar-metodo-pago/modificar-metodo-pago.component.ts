6fb6c042e8aab48ed67261a87a7834cefc9e5b9e
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-11-17 18:33:28 -0600

    Actualización Modificar Pago

--- projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/modificar-metodo-pago/modificar-metodo-pago.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/modificar-metodo-pago/modificar-metodo-pago.component.ts
@@ -1,24 +1,29 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {ActivatedRoute, Router} from "@angular/router";
-import {OverlayPanel} from "primeng/overlaypanel";
+import {DetallePago, MetodoPago} from "../../modelos/detallePago.interface";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {TipoDropdown} from "../../../../../models/tipo-dropdown";
+import {TIPO_PAGO_CATALOGOS_CONVENIO, TIPO_PAGO_CATALOGOS_ODS} from "../../constants/catalogos";
+import {FormBuilder, FormGroup} from "@angular/forms";
 import {DialogService, DynamicDialogConfig} from "primeng/dynamicdialog";
 import {MAX_WIDTH} from "../../../../../utils/constantes";
+import {OverlayPanel} from "primeng/overlaypanel";
+import {ParametrosEliminar, ParametrosModificar, RegistroModal} from "../../modelos/datosRegistro.interface";
+import {RegistrarAgfComponent} from "../registrar-pago/registrar-agf/registrar-agf.component";
+import {RegistrarTipoPagoComponent} from "../registrar-pago/registrar-tipo-pago/registrar-tipo-pago.component";
+import {
+  RegistrarValeParitariaComponent
+} from "../registrar-pago/registrar-vale-paritaria/registrar-vale-paritaria.component";
 import {ModificarTipoPagoComponent} from "../modificar-tipo-pago/modificar-tipo-pago.component";
 import {EliminarTipoPagoComponent} from "../eliminar-tipo-pago/eliminar-tipo-pago.component";
-import {DetallePago, MetodoPago} from "../../modelos/detallePago.interface";
 
-interface ParametrosModificar {
-  metodoPago: string,
-  tipoPago: string,
-  importe: number,
-  idPagoDetalle: number
+interface DialogoAGF {
+  idFinado: number,
+  idPagoBitacora: number,
+  idFlujoPago: number,
+  idRegistro: number,
+  importePago: number,
 }
-
-interface ParametrosCancelar {
-  pago: MetodoPago,
-  total: number
-}
-
 @Component({
   selector: 'app-modificar-metodo-pago',
   templateUrl: './modificar-metodo-pago.component.html',
@@ -33,46 +38,186 @@ export class ModificarMetodoPagoComponent implements OnInit {
   registroPago!: DetallePago;
   pagoSeleccionado!: MetodoPago;
 
+  idPagoBitacora!: number;
+  fecha: Date = new Date();
+  tipoPago: string = '';
+  tipoFolio: string = '';
+  tiposPago: TipoDropdown[] = [];
+  pagoForm!: FormGroup;
+
   constructor(
-    private router: Router,
+    private formBuilder: FormBuilder,
     private readonly activatedRoute: ActivatedRoute,
+    private location: Location,
     public dialogService: DialogService,
   ) {
   }
 
+  ngOnInit(): void {
+    this.inicializarFormPago();
+    this.cargarCatalogos();
+  }
+
+  inicializarFormPago(): void {
+    this.pagoForm = this.formBuilder.group({
+      tipoPago: [{value: null, disabled: false}]
+    });
+  }
+
+  cargarCatalogos(): void {
+    this.registroPago = this.activatedRoute.snapshot.data["respuesta"].datos;
+    this.tipoPago = this.obtenerTipoPago();
+    this.tipoFolio = this.obtenerFolioTipoPago();
+    this.tiposPago = this.obtenerMetodosPago();
+    this.idPagoBitacora = this.activatedRoute.snapshot.paramMap.get('idPagoBitacora') as unknown as number;
+    this.actualizarValidaciones();
+  }
+
+  obtenerFolioTipoPago(): string {
+    if (this.registroPago.tipoPago === 'Pago de Orden de Servicio') {
+      return 'Folio ODS'
+    }
+    if (this.registroPago.tipoPago === 'Pago de Nuevos Convenios de Previsión Funeraria') {
+      return 'Folio NCPF';
+    }
+    return 'Folio PRCPF';
+  }
+
+  obtenerTipoPago(): string {
+    if (this.registroPago.tipoPago === 'Pago de Orden de Servicio') {
+      return 'Pago de orden de servicio'
+    }
+    if (this.registroPago.tipoPago === 'Pago de Nuevos Convenios de Previsión Funeraria') {
+      return 'Pago de Nuevos convenios de previsión funeraria';
+    }
+    return 'Pago de Renovación de convenios de previsión funeraria';
+  }
+
+  obtenerMetodosPago(): TipoDropdown[] {
+    if (this.registroPago.tipoPago === 'Pago de Orden de Servicio') {
+      return this.verificarValeParitaria();
+    }
+    if (this.registroPago.tipoPago === 'Pago de Nuevos Convenios de Previsión Funeraria') {
+      return TIPO_PAGO_CATALOGOS_CONVENIO;
+    }
+    return TIPO_PAGO_CATALOGOS_CONVENIO;
+  }
+
+  verificarValeParitaria(): TipoDropdown[] {
+    const tipoPago: TipoDropdown[] = [...TIPO_PAGO_CATALOGOS_ODS];
+    if (this.registroPago.valeP > 0 || this.registroPago.nss) {
+      tipoPago.shift()
+    }
+    return tipoPago;
+  }
+
+  seleccionarPago(): void {
+    const tipoPago: number = this.pagoForm.get('tipoPago')?.value as number;
+    if (tipoPago === 1) {
+      this.abrirModalValeParitaria();
+      return;
+    }
+    if (tipoPago === 2) {
+      this.abrirModalAGF();
+      return;
+    }
+    this.registrarPago();
+  }
+
+  registrarPago(): void {
+    const idPago = this.pagoForm.get('tipoPago')?.value;
+    const tipoPago: string = this.obtenerTipoPagoSeleccionado(idPago);
+    const data: RegistroModal = this.generarDatosDialogo();
+    const REGISTRAR_PAGO_CONFIG: DynamicDialogConfig = this.crearConfiguracionDialogo(`Registro de ${tipoPago.toLowerCase()}`, data)
+    this.dialogService.open(RegistrarTipoPagoComponent, REGISTRAR_PAGO_CONFIG);
+  }
+
+  obtenerTipoPagoSeleccionado(idPago: number): string {
+    const tipoPago: string | undefined = this.tiposPago.find((tp: TipoDropdown) => tp.value === idPago)?.label
+    return tipoPago ?? ''
+  }
+
+  crearConfiguracionDialogo(header: string, data: RegistroModal | ParametrosEliminar | ParametrosModificar | DialogoAGF): DynamicDialogConfig {
+    return {header, width: MAX_WIDTH, data}
+  }
+
+  abrirModalValeParitaria(): void {
+    const data: RegistroModal = this.generarDatosDialogo();
+    const REGISTRAR_PAGO_CONFIG: DynamicDialogConfig = this.crearConfiguracionDialogo('Registro de Vale Paritaria', data);
+    this.dialogService.open(RegistrarValeParitariaComponent, REGISTRAR_PAGO_CONFIG)
+  }
+
+  generarDatosDialogo(): RegistroModal {
+    const idPago = this.pagoForm.get('tipoPago')?.value;
+    const tipoPago: string = this.tiposPago.find((tp: TipoDropdown) => tp.value === idPago)?.label ?? '';
+    return {
+      tipoPago, idPago,
+      total: this.registroPago.totalPorCubrir,
+      datosRegistro: {
+        idPagoBitacora: this.registroPago.idPagoBitacora,
+        idFlujoPago: this.registroPago.idFlujoPago,
+        idRegistro: this.registroPago.idRegistro,
+      }
+    }
+  }
+
+  regresarPaginaPrevia(): void {
+    this.location.back();
+  }
+
   mostrarOverlay(event: MouseEvent, pago: MetodoPago): void {
     event.stopPropagation();
-    this.overlayPanel.toggle(event);
     this.pagoSeleccionado = pago;
+    this.overlayPanel.toggle(event);
   }
 
   modificarTipoPago(): void {
-    const data: ParametrosModificar = {
-      metodoPago: this.pagoSeleccionado.metodoPago, importe: this.pagoSeleccionado.importe,
-      tipoPago: this.registroPago.tipoPago, idPagoDetalle: this.pagoSeleccionado.idPagoDetalle
-    };
-    const MODIFICAR_TIPO_PAGO_CONFIG: DynamicDialogConfig = {
-      header: "Modificar pago",
-      width: MAX_WIDTH,
-      data
-    };
+    const data: ParametrosModificar = this.generarDatosDialogoModificar();
+    const MODIFICAR_TIPO_PAGO_CONFIG: DynamicDialogConfig = this.crearConfiguracionDialogo('Modificar metodo de pago', data);
     this.dialogService.open(ModificarTipoPagoComponent, MODIFICAR_TIPO_PAGO_CONFIG)
   }
 
-  ngOnInit(): void {
-    this.registroPago = this.activatedRoute.snapshot.data["respuesta"].datos;
-  }
-
-  cancelarTipoPago(): void {
-    const data: ParametrosCancelar = {
-      pago: this.pagoSeleccionado,
+  generarDatosDialogoModificar(): ParametrosModificar {
+    return {
+      metodoPago: this.pagoSeleccionado.metodoPago, importe: this.pagoSeleccionado.importe,
+      tipoPago: this.registroPago.tipoPago, idPagoDetalle: this.pagoSeleccionado.idPagoDetalle,
       total: this.registroPago.totalAPagar
     };
-    const CANCELAR_TIPO_PAGO_CONFIG: DynamicDialogConfig = {
-      header: "Cancelar pago",
-      width: MAX_WIDTH,
-      data
-    };
+  }
+
+  eliminarTipoPago(): void {
+    const data: ParametrosEliminar = this.generarDatosDialogoEliminar();
+    const CANCELAR_TIPO_PAGO_CONFIG: DynamicDialogConfig = this.crearConfiguracionDialogo('Eliminar método de pago registrado', data);
     this.dialogService.open(EliminarTipoPagoComponent, CANCELAR_TIPO_PAGO_CONFIG);
   }
+
+  generarDatosDialogoEliminar(): ParametrosEliminar {
+    return {
+      pago: this.pagoSeleccionado,
+      total: this.registroPago.totalAPagar
+    }
+  }
+
+  actualizarValidaciones(): void {
+    if (this.registroPago.totalPorCubrir > 0) return;
+    this.pagoForm.get('tipoPago')?.disable();
+  }
+
+  abrirModalAGF(): void {
+    const data: DialogoAGF = this.generarDatosDialogoAGF();
+    const REGISTRAR_PAGO_CONFIG: DynamicDialogConfig = this.crearConfiguracionDialogo("Registro de Ayuda de Gastos de Funeral", data);
+    this.dialogService.open(RegistrarAgfComponent, REGISTRAR_PAGO_CONFIG);
+  }
+
+  generarDatosDialogoAGF(): DialogoAGF {
+    return {
+      idFinado: this.registroPago.idFinado,
+      idPagoBitacora: this.registroPago.idPagoBitacora,
+      idFlujoPago: this.registroPago.idFlujoPago,
+      idRegistro: this.registroPago.idRegistro,
+      importePago: this.registroPago.totalPorCubrir,
+    }
+  }
+
+
 }
