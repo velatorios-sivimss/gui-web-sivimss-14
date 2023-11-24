bea1fcecfc77cd2d82a24eadd6df73cd63d706b9
GRUPOPM\mparedes <martin.paredes@people-media.com.mx>
2023-11-24 07:54:04 -0600

    Se agrega url de nuevo login contratante en enviroments

--- projects/sivimss-gui/src/environments/environment.ts
+++ projects/sivimss-gui/src/environments/environment.ts
@@ -14,6 +14,7 @@ export const environment = {
 
     // mssivimss: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ctrol-permisos/v1/sivimss/service/',
     // login: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-oauth/v1',
+    // loginContratante: 'http://localhost:8085/mssivimss-oauth/v1',
     // servicios_externos: 'http://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ser-externos/v1/catalogos/externos/',
     // servicios_externos_reportes: 'http://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ser-externos/v1/reportes/',
     // notificaciones: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-notificaciones/v1/notificaciones/avisos',
