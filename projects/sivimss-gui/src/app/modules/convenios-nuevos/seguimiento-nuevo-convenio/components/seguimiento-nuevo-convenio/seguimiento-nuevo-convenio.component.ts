050aa850f0b14ff32820e900c4eccfdfd763ed54
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-12-11 10:57:23 -0600

    Paginación Nuevo Convenio

--- projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/components/seguimiento-nuevo-convenio/seguimiento-nuevo-convenio.component.ts
+++ projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/components/seguimiento-nuevo-convenio/seguimiento-nuevo-convenio.component.ts
@@ -1,11 +1,10 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, FormGroupDirective, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {LazyLoadEvent} from 'primeng/api';
 import {DialogService, DynamicDialogRef} from 'primeng/dynamicdialog';
 import {OverlayPanel} from 'primeng/overlaypanel';
 import {TipoDropdown} from 'projects/sivimss-gui/src/app/models/tipo-dropdown';
-import {AlertaService} from 'projects/sivimss-gui/src/app/shared/alerta/services/alerta.service';
 import {BreadcrumbService} from 'projects/sivimss-gui/src/app/shared/breadcrumb/services/breadcrumb.service';
 import {DIEZ_ELEMENTOS_POR_PAGINA} from 'projects/sivimss-gui/src/app/utils/constantes';
 import {CATALOGOS_DUMMIES} from '../../../../proveedores/constants/dummies';
@@ -14,222 +13,211 @@ import {SEGUIMIENTO_CONVENIO_BREADCRUMB} from "../../constants/breadcrumb";
 import {UsuarioEnSesion} from "../../../../../models/usuario-en-sesion.interface";
 import {HttpRespuesta} from "../../../../../models/http-respuesta.interface";
 import {
-    mapearArregloTipoDropdown,
-    obtenerNivelUsuarioLogueado,
-    obtenerVelatorioUsuarioLogueado
+  mapearArregloTipoDropdown,
+  obtenerNivelUsuarioLogueado,
+  obtenerVelatorioUsuarioLogueado, validarUsuarioLogueado
 } from "../../../../../utils/funciones";
 import {HttpErrorResponse} from "@angular/common/http";
 import {SeguimientoNuevoConvenioService} from "../../services/seguimiento-nuevo-convenio.service";
+import {MensajesSistemaService} from "../../../../../services/mensajes-sistema.service";
+import {finalize} from "rxjs/operators";
+import {LoaderService} from "../../../../../shared/loader/services/loader.service";
 
 @Component({
-    selector: 'app-seguimiento-nuevo-convenio',
-    templateUrl: './seguimiento-nuevo-convenio.component.html',
-    styleUrls: ['./seguimiento-nuevo-convenio.component.scss'],
-    providers: [DialogService],
+  selector: 'app-seguimiento-nuevo-convenio',
+  templateUrl: './seguimiento-nuevo-convenio.component.html',
+  styleUrls: ['./seguimiento-nuevo-convenio.component.scss'],
+  providers: [DialogService],
 })
 export class SeguimientoNuevoConvenioComponent implements OnInit {
+  @ViewChild(OverlayPanel)
+  overlayPanel!: OverlayPanel;
 
-    @ViewChild(OverlayPanel)
-    overlayPanel!: OverlayPanel;
+  @ViewChild(FormGroupDirective)
+  private filtroFormDir!: FormGroupDirective;
 
-    catalogoNiveles: TipoDropdown[] = [];
-    catalogoVelatorios: TipoDropdown[] = [];
+  catalogoNiveles: TipoDropdown[] = [];
+  catalogoVelatorios: TipoDropdown[] = [];
 
-    numPaginaActual: number = 0;
-    cantElementosPorPagina: number = DIEZ_ELEMENTOS_POR_PAGINA;
-    totalElementos: number = 0;
+  numPaginaActual: number = 0;
+  cantElementosPorPagina: number = DIEZ_ELEMENTOS_POR_PAGINA;
+  totalElementos: number = 0;
 
-    central!: boolean;
+  central!: boolean;
 
-    convenios: SeguimientoNuevoConvenio[] = [];
-    convenioSeleccionado: SeguimientoNuevoConvenio = {};
+  convenios: SeguimientoNuevoConvenio[] = [];
+  convenioSeleccionado: SeguimientoNuevoConvenio = {};
 
-    filtroForm!: FormGroup;
+  filtroForm!: FormGroup;
+  paginacionConFiltrado: boolean = false;
 
-    creacionRef!: DynamicDialogRef;
-    detalleRef!: DynamicDialogRef;
-    modificacionRef!: DynamicDialogRef;
+  creacionRef!: DynamicDialogRef;
+  detalleRef!: DynamicDialogRef;
+  modificacionRef!: DynamicDialogRef;
 
-    opciones: TipoDropdown[] = CATALOGOS_DUMMIES;
-    tipoServicio: TipoDropdown[] = CATALOGOS_DUMMIES;
-    partidaPresupuestal: TipoDropdown[] = CATALOGOS_DUMMIES;
-    cuentaContable: TipoDropdown[] = CATALOGOS_DUMMIES;
+  opciones: TipoDropdown[] = CATALOGOS_DUMMIES;
+  tipoServicio: TipoDropdown[] = CATALOGOS_DUMMIES;
+  partidaPresupuestal: TipoDropdown[] = CATALOGOS_DUMMIES;
+  cuentaContable: TipoDropdown[] = CATALOGOS_DUMMIES;
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private breadcrumbService: BreadcrumbService,
+    public dialogService: DialogService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private cargadorService: LoaderService,
+    private seguimientoConvenioService: SeguimientoNuevoConvenioService,
+    private mensajesSistemaService: MensajesSistemaService
+  ) {
+  }
 
-    constructor(
-        private formBuilder: FormBuilder,
-        private breadcrumbService: BreadcrumbService,
-        private alertaService: AlertaService,
-        public dialogService: DialogService,
-        private router: Router,
-        private activatedRoute: ActivatedRoute,
-        private seguimientoConvenioService: SeguimientoNuevoConvenioService
-    ) {
+  ngOnInit(): void {
+    this.breadcrumbService.actualizar(SEGUIMIENTO_CONVENIO_BREADCRUMB);
+    this.cargarCatalogos();
+    this.inicializarFiltroForm();
+  }
+
+  cargarCatalogos(): void {
+    this.catalogoNiveles = this.activatedRoute.snapshot.data["respuesta"];
+    this.obtenerVelatorios();
+  }
+
+  inicializarFiltroForm(): void {
+    const usuario: UsuarioEnSesion = JSON.parse(localStorage.getItem('usuario') as string);
+    this.central = obtenerNivelUsuarioLogueado(usuario) === 1;
+    this.filtroForm = this.formBuilder.group({
+      nivel: [{value: obtenerNivelUsuarioLogueado(usuario), disabled: true}],
+      velatorio: [{
+        value: this.central ? null : obtenerVelatorioUsuarioLogueado(usuario),
+        disabled: obtenerNivelUsuarioLogueado(usuario) === 3
+      }],
+      folioConvenioPf: [{value: null, disabled: false}, [Validators.required]],
+      folioConvenioPsfpa: [{value: null, disabled: false}, [Validators.required]],
+      rfcAfiliado: [{value: null, disabled: false}, [Validators.required]],
+    });
+  }
+
+  obtenerVelatorios(): void {
+    const usuario: UsuarioEnSesion = JSON.parse(localStorage.getItem('usuario') as string);
+    const delegacion: null | string = this.central ? null : usuario?.idDelegacion ?? null;
+    this.seguimientoConvenioService.obtenerVelatoriosPorDelegacion(delegacion).subscribe({
+      next: (respuesta: HttpRespuesta<any>): void => this.solicitudExitosaVelatorios(respuesta),
+      error: (error: HttpErrorResponse): void => this.manejarMensajeError(error)
+    });
+  }
+
+  solicitudExitosaVelatorios(respuesta: HttpRespuesta<any>): void {
+    this.catalogoVelatorios = mapearArregloTipoDropdown(respuesta.datos, "desc", "id");
+  }
+
+  private manejarMensajeError(error: HttpErrorResponse): void {
+    console.error(error);
+    this.mensajesSistemaService.mostrarMensajeError(error);
+  }
+
+  seleccionarPaginacion(event?: LazyLoadEvent): void {
+    if (validarUsuarioLogueado()) return;
+    if (event) {
+      this.numPaginaActual = Math.floor((event.first ?? 0) / (event.rows ?? 1));
     }
-
-    ngOnInit(): void {
-        this.breadcrumbService.actualizar(SEGUIMIENTO_CONVENIO_BREADCRUMB);
-        this.inicializarFiltroForm();
-        this.cargarCatalogos();
+    if (this.paginacionConFiltrado) {
+      // this.paginarConFiltros();
+    } else {
+      this.paginar();
     }
+  }
 
-    cargarCatalogos(): void {
-        this.catalogoNiveles = this.activatedRoute.snapshot.data["respuesta"];
-        this.obtenerVelatorios();
-    }
+  paginar(): void {
+    this.cargadorService.activar();
+    this.seguimientoConvenioService
+      .buscarPorFiltros({ idVelatorio: null }, this.numPaginaActual, this.cantElementosPorPagina)
+      .pipe(finalize(() => this.cargadorService.desactivar()))
+      .subscribe({
+        next: (respuesta: HttpRespuesta<any>): void => this.procesarRespuestaPaginacion(respuesta),
+        error: (error: HttpErrorResponse): void => this.manejarMensajeError(error)
+      });
+  }
 
-    inicializarFiltroForm(): void {
-        const usuario: UsuarioEnSesion = JSON.parse(localStorage.getItem('usuario') as string);
-        this.central = obtenerNivelUsuarioLogueado(usuario) === 1;
-        this.filtroForm = this.formBuilder.group({
-            nivel: [{value: obtenerNivelUsuarioLogueado(usuario), disabled: true}],
-            velatorio: [{
-                value: this.central ? null : obtenerVelatorioUsuarioLogueado(usuario),
-                disabled: obtenerNivelUsuarioLogueado(usuario) === 3
-            }],
-            folioConvenioPf: [{value: null, disabled: false}, [Validators.required]],
-            folioConvenioPsfpa: [{value: null, disabled: false}, [Validators.required]],
-            rfcAfiliado: [{value: null, disabled: false}, [Validators.required]],
-        });
-    }
+  procesarRespuestaPaginacion(respuesta: HttpRespuesta<any>): void {
+    this.convenios = respuesta.datos.content;
+    this.totalElementos = respuesta.datos.totalElements;
+  }
 
-    obtenerVelatorios(): void {
-        const usuario: UsuarioEnSesion = JSON.parse(localStorage.getItem('usuario') as string);
-        const delegacion: null | string = this.central ? null : usuario?.idDelegacion ?? null;
-        this.seguimientoConvenioService.obtenerVelatoriosPorDelegacion(delegacion).subscribe({
-            next: (respuesta: HttpRespuesta<any>): void => this.solicitudExitosaVelatorios(respuesta),
-            error: (error: HttpErrorResponse): void => {
-                console.error("ERROR: ", error);
-            }
-        });
-    }
+  limpiar(): void {
+    this.filtroForm.reset();
+  }
 
-    solicitudExitosaVelatorios(respuesta: HttpRespuesta<any>): void {
-        this.catalogoVelatorios = mapearArregloTipoDropdown(respuesta.datos, "desc", "id");
-    }
+  buscar(): void {
 
-    paginar(event: LazyLoadEvent): void {
-        console.log(event);
-        setTimeout(() => {
-            this.convenios = [
-                {
-                    id: 1,
-                    nivel: "ataúd",
-                    velatorio: "Artículo complementario",
-                    folioConvenioPf: "Madera ecológica MDF",
-                    folioConvenioPsfpa: "Tambora",
-                    rfcAfiliado: "Intermediaria",
-                    tipoContratacion: "Por persona",
-                    rfc: "fjdgf639d ",
-                    nombreAfiliado: "10m",
-                    tipoPaquete: "2m",
-                    estatus: true,
-                },
-                {
-                    id: 2,
-                    nivel: "ataúd",
-                    velatorio: "Artículo complementario",
-                    folioConvenioPf: "Madera ecológica MDF",
-                    folioConvenioPsfpa: "Tambora",
-                    rfcAfiliado: "Intermediaria",
-                    tipoContratacion: "Por Empresa",
-                    rfc: "fjdgf639d ",
-                    nombreAfiliado: "10m",
-                    tipoPaquete: "2m",
-                    estatus: true,
-                },
-                {
-                    id: 3,
-                    nivel: "ataúd",
-                    velatorio: "Artículo complementario",
-                    folioConvenioPf: "Madera ecológica MDF",
-                    folioConvenioPsfpa: "Tambora",
-                    rfcAfiliado: "Intermediaria",
-                    tipoContratacion: "Numero de folio de convenio (Pre registro)",
-                    rfc: "fjdgf639d ",
-                    nombreAfiliado: "10m",
-                    tipoPaquete: "2m",
-                    estatus: true,
-                }
-            ];
-            this.totalElementos = this.convenios.length;
-        }, 0)
-    }
+  }
 
-    limpiar(): void {
-        this.filtroForm.reset();
-    }
+  abrirModalAgregarServicio(): void {
+    // this.creacionRef = this.dialogService.open(AgregarArticulosComponent,{
+    //   header:"Agregar artículo",
+    //   width:"920px"
+    // });
+    // this.creacionRef.onClose.subscribe((estatus:boolean) => {
+    //   if(estatus){
+    //     this.alertaService.mostrar(TipoAlerta.Exito, 'Artículo agregado correctamente');
+    //   }
+    // })
+  }
 
-    buscar(): void {
+  abrirCambioEstatus(servicio: SeguimientoNuevoConvenio) {
+    void this.router.navigate(['desactivar-convenio'], {relativeTo: this.activatedRoute});
+    /*Preguntar si se puede usar 'let'*/
+    // let header:string = "" ;
+    // servicio.estatus?header="Activar artículo":header="Desactivar artículo";
+    // this.creacionRef = this.dialogService.open(DetalleArticulosComponent, {
+    //   header:header,
+    //   width:"920px",
+    //   data: {servicio:servicio, origen: "estatus"},
+    // })
 
-    }
+    // this.creacionRef.onClose.subscribe((servicio:Articulos) => {
+    //   if(servicio.estatus){
+    //     this.alertaService.mostrar(TipoAlerta.Exito, 'Artículo activado correctamente');
+    //   }else{
+    //     this.alertaService.mostrar(TipoAlerta.Exito, 'Servicio desactivado correctamente');
+    //   }
+    // })
 
-    abrirModalAgregarServicio(): void {
-        // this.creacionRef = this.dialogService.open(AgregarArticulosComponent,{
-        //   header:"Agregar artículo",
-        //   width:"920px"
-        // });
-        // this.creacionRef.onClose.subscribe((estatus:boolean) => {
-        //   if(estatus){
-        //     this.alertaService.mostrar(TipoAlerta.Exito, 'Artículo agregado correctamente');
-        //   }
-        // })
-    }
+  }
 
-    abrirCambioEstatus(servicio: SeguimientoNuevoConvenio) {
-        void this.router.navigate(['desactivar-convenio'], {relativeTo: this.activatedRoute});
-        /*Preguntar si se puede usar 'let'*/
-        // let header:string = "" ;
-        // servicio.estatus?header="Activar artículo":header="Desactivar artículo";
-        // this.creacionRef = this.dialogService.open(DetalleArticulosComponent, {
-        //   header:header,
-        //   width:"920px",
-        //   data: {servicio:servicio, origen: "estatus"},
-        // })
+  abrirConcluirServicio(): void {
+    // this.creacionRef = this.dialogService.open(ModificarArticulosComponent, {
+    //   header:"Modificar artículo",
+    //   width:"920px",
+    // })
 
-        // this.creacionRef.onClose.subscribe((servicio:Articulos) => {
-        //   if(servicio.estatus){
-        //     this.alertaService.mostrar(TipoAlerta.Exito, 'Artículo activado correctamente');
-        //   }else{
-        //     this.alertaService.mostrar(TipoAlerta.Exito, 'Servicio desactivado correctamente');
-        //   }
-        // })
+    // this.creacionRef.onClose.subscribe((estatus:boolean) => {
+    //   if(estatus){
+    //     this.alertaService.mostrar(TipoAlerta.Exito, 'Artículo modificado correctamente');
+    //   }
+    // })
+  }
 
-    }
+  abrirPanel(event: MouseEvent, convenioSeleccionado: SeguimientoNuevoConvenio): void {
+    this.convenioSeleccionado = convenioSeleccionado;
+    this.overlayPanel.toggle(event);
+  }
 
-    abrirConcluirServicio(): void {
-        // this.creacionRef = this.dialogService.open(ModificarArticulosComponent, {
-        //   header:"Modificar artículo",
-        //   width:"920px",
-        // })
+  abrirModalDetalleCapilla(servicio: SeguimientoNuevoConvenio) {
+    // this.creacionRef = this.dialogService.open(DetalleArticulosComponent, {
+    //   header:"Detalle",
+    //   width:"920px",
+    //   data: {servicio:servicio, origen: "detalle"},
+    // })
+  }
 
-        // this.creacionRef.onClose.subscribe((estatus:boolean) => {
-        //   if(estatus){
-        //     this.alertaService.mostrar(TipoAlerta.Exito, 'Artículo modificado correctamente');
-        //   }
-        // })
-    }
+  consultaServicioEspecifico(): string {
+    return ''
+  }
 
-    abrirPanel(event: MouseEvent, convenioSeleccionado: SeguimientoNuevoConvenio): void {
-        this.convenioSeleccionado = convenioSeleccionado;
-        this.overlayPanel.toggle(event);
-    }
-
-    abrirModalDetalleCapilla(servicio: SeguimientoNuevoConvenio) {
-        // this.creacionRef = this.dialogService.open(DetalleArticulosComponent, {
-        //   header:"Detalle",
-        //   width:"920px",
-        //   data: {servicio:servicio, origen: "detalle"},
-        // })
-    }
-
-    consultaServicioEspecifico(): string {
-        return ''
-    }
-
-    abrirPreRegistroNuevoConvenio(): void {
-        this.router.navigate(['pre-registro-nuevo-convenio'], {relativeTo: this.activatedRoute});
-    }
+  abrirPreRegistroNuevoConvenio(): void {
+    this.router.navigate(['pre-registro-nuevo-convenio'], {relativeTo: this.activatedRoute});
+  }
 
 
 }
--- projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/services/seguimiento-nuevo-convenio.service.ts
+++ projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/services/seguimiento-nuevo-convenio.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {Observable, of} from "rxjs";
 import {TipoDropdown} from "../../../../models/tipo-dropdown";
 import {mapearArregloTipoDropdown} from "../../../../utils/funciones";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpParams} from "@angular/common/http";
 import {AutenticacionService} from "../../../../services/autenticacion.service";
 import {environment} from "../../../../../environments/environment";
 import {BaseService} from "../../../../utils/base-service";
@@ -11,18 +11,27 @@ import {HttpRespuesta} from "../../../../models/http-respuesta.interface";
 @Injectable()
 export class SeguimientoNuevoConvenioService extends BaseService<HttpRespuesta<any>, any> {
 
-    constructor(override _http: HttpClient, private authService: AutenticacionService) {
-        super(_http, `${environment.api.mssivimss}`, "", "",
-            0, "", "", "");
-    }
+  constructor(override _http: HttpClient, private authService: AutenticacionService) {
+    super(_http, `${environment.api.mssivimss}`, "", "",
+      77, "", "", "");
+  }
 
-    obtenerCatalogoNiveles(): Observable<TipoDropdown[]> {
-        const niveles = this.authService.obtenerCatalogoDeLocalStorage(('catalogo_nivelOficina'));
-        return of(mapearArregloTipoDropdown(niveles, "desc", "id"));
-    }
+  base: string = 'http://localhost:8077/mssivimss-pre-reg-conven/v1/sivimss';
+  _filtros: string = 'preregistros';
 
-    obtenerVelatoriosPorDelegacion(delegacion: string | null = null): Observable<HttpRespuesta<any>> {
-        const body: { idDelegacion: string | null } = {idDelegacion: delegacion}
-        return this._http.post<HttpRespuesta<any>>(`${environment.api.login}/velatorio/consulta`, body);
-    }
+  obtenerCatalogoNiveles(): Observable<TipoDropdown[]> {
+    const niveles = this.authService.obtenerCatalogoDeLocalStorage(('catalogo_nivelOficina'));
+    return of(mapearArregloTipoDropdown(niveles, "desc", "id"));
+  }
+
+  obtenerVelatoriosPorDelegacion(delegacion: string | null = null): Observable<HttpRespuesta<any>> {
+    const body: { idDelegacion: string | null } = {idDelegacion: delegacion}
+    return this._http.post<HttpRespuesta<any>>(`${environment.api.login}/velatorio/consulta`, body);
+  }
+
+  buscarPorFiltros(filtros: any, pagina: number, tamanio: number): Observable<HttpRespuesta<any>> {
+    const body = {pagina, tamanio, ...filtros}
+    return this._http.post<HttpRespuesta<any>>(this.base + `/buscar/${this._filtros}`,
+      body);
+  }
 }
