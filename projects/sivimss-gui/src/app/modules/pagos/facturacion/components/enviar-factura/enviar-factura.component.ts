403ee02768f1fbd10b3c85e4465eaa6ea37b1a19
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-12-15 17:49:59 -0600

    Bingdin datos Enviar Factura

--- projects/sivimss-gui/src/app/modules/pagos/facturacion/components/enviar-factura/enviar-factura.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/facturacion/components/enviar-factura/enviar-factura.component.ts
@@ -1,5 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute} from "@angular/router";
+import {PATRON_CORREO} from "../../../../../utils/constantes";
+
+interface ParamsEnviar {
+  folioFactura: number,
+  folioFiscal: string,
+  folioRelacionado: string
+}
 
 @Component({
   selector: 'app-enviar-factura',
@@ -10,19 +18,31 @@ export class EnviarFacturaComponent implements OnInit {
 
   enviarForm!: FormGroup;
   mostrarDialogEnviar: boolean = false;
+  registroEnviar!: ParamsEnviar;
 
   constructor(
+    private readonly activatedRoute: ActivatedRoute,
     private formBuilder: FormBuilder,
   ) {
+    this.obtenerParametrosEnviar();
   }
 
   ngOnInit(): void {
     this.inicializarEnviarForm();
   }
 
+  obtenerParametrosEnviar(): void {
+    this.activatedRoute.queryParams.pipe(
+    ).subscribe(params => {
+        const {datos_enviar} = params;
+        this.registroEnviar = JSON.parse(window.atob(datos_enviar));
+      }
+    );
+  }
+
   inicializarEnviarForm(): void {
     this.enviarForm = this.formBuilder.group({
-      correoElectronico: [{value: null, disabled: false}, [Validators.required]],
+      correoElectronico: [{value: null, disabled: false}, [Validators.required, Validators.email, Validators.pattern(PATRON_CORREO)]],
     });
   }
 
