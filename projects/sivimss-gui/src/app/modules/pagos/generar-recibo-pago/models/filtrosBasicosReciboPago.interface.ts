daa10d7ff7f94a62b3de587c84daa07eb50fd111
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-12-19 13:13:36 -0600

    Limpieza Recibo de Pago base

---
+++ projects/sivimss-gui/src/app/modules/pagos/generar-recibo-pago/models/filtrosBasicosReciboPago.interface.ts
@@ -0,0 +1,11 @@
+export interface FiltrosBasicosReciboPago {
+  idNivel: number,
+  idDelegacion:  number | null,
+  idVelatorio:  number | null,
+}
+
+export interface FormBasicoReciboPago {
+  nivel: number,
+  delegacion:  number | null,
+  velatorio:  number | null,
+}
