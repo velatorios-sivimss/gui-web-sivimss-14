d77d7738aeddc3ad8a36832397a4376bb3d4cbe8
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-12-20 12:03:29 -0600

    Formulario Standalone Beneficiario

--- projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/shared/datos-beneficiario/datos-beneficiario.component.ts
+++ projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/shared/datos-beneficiario/datos-beneficiario.component.ts
@@ -1,13 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {ControlContainer, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-datos-beneficiario',
   templateUrl: './datos-beneficiario.component.html',
-  styleUrls: ['./datos-beneficiario.component.scss']
+  styleUrls: ['./datos-beneficiario.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  viewProviders: [
+    {
+      provide: ControlContainer,
+      useFactory: () => inject(ControlContainer, {skipSelf: true})
+    }
+  ]
 })
 export class DatosBeneficiarioComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
