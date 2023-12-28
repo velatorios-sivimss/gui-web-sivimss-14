031deaecc168d3b3178d83132a1cc7e6b242f174
epina-gdc <eduardo.gomez@people-media.com.mx>
2023-12-28 09:53:39 -0600

    Agregados AGF y Vale Paritaria

--- projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/control-pago/control-pago.component.ts
+++ projects/sivimss-gui/src/app/modules/pagos/realizar-pago/components/control-pago/control-pago.component.ts
@@ -16,6 +16,11 @@ import {
 } from "../registrar-pago/registrar-vale-paritaria/registrar-vale-paritaria.component";
 import {ParametrosEliminar, ParametrosModificar, RegistroModal} from "../../modelos/datosRegistro.interface";
 import {RegistrarAgfComponent} from "../registrar-pago/registrar-agf/registrar-agf.component";
+import {HttpRespuesta} from "../../../../../models/http-respuesta.interface";
+import {forkJoin, Observable} from "rxjs";
+import {finalize} from "rxjs/operators";
+import {LoaderService} from "../../../../../shared/loader/services/loader.service";
+import {RealizarPagoService} from "../../services/realizar-pago.service";
 
 interface DialogoAGF {
   idFinado: number,
@@ -23,6 +28,13 @@ interface DialogoAGF {
   idFlujoPago: number,
   idRegistro: number,
   importePago: number,
+}
+
+interface RespuestaAGF {
+  idODS: number,
+  agf: number,
+  nss: null | number,
+  idFinado: null | number
 }
 
 @Component({
@@ -46,12 +58,15 @@ export class ControlPagoComponent implements OnInit {
   titulo: string = '';
   tiposPago: TipoDropdown[] = [];
   pagoForm!: FormGroup;
+  agfSeleccionado!: RespuestaAGF;
 
   constructor(
     private formBuilder: FormBuilder,
     private readonly activatedRoute: ActivatedRoute,
     private location: Location,
+    private realizarPagoService: RealizarPagoService,
     public dialogService: DialogService,
+    private cargadorService: LoaderService,
   ) {
   }
 
@@ -71,7 +86,7 @@ export class ControlPagoComponent implements OnInit {
     this.tipoPago = this.obtenerTipoPago();
     this.titulo = this.obtenerTipoPago();
     this.tipoFolio = this.obtenerFolioTipoPago();
-    this.tiposPago = this.obtenerMetodosPago();
+    this.obtenerMetodosPago();
     this.idPagoBitacora = this.activatedRoute.snapshot.paramMap.get('idPagoBitacora') as unknown as number;
     this.actualizarValidaciones();
   }
@@ -96,22 +111,47 @@ export class ControlPagoComponent implements OnInit {
     return 'Pago de Renovación de convenios de previsión funeraria';
   }
 
-  obtenerMetodosPago(): TipoDropdown[] {
+  obtenerMetodosPago(): void {
     if (this.registroPago.tipoPago === 'Pago de Orden de Servicio') {
-      return this.verificarValeParitaria();
+      this.filtrarCatalogosODS();
     }
     if (this.registroPago.tipoPago === 'Pago de Nuevos Convenios de Previsión Funeraria') {
-      return TIPO_PAGO_CATALOGOS_CONVENIO;
+      this.tiposPago = TIPO_PAGO_CATALOGOS_CONVENIO;
     }
-    return TIPO_PAGO_CATALOGOS_CONVENIO;
+    this.tiposPago = TIPO_PAGO_CATALOGOS_CONVENIO;
   }
 
-  verificarValeParitaria(): TipoDropdown[] {
-    const tipoPago: TipoDropdown[] = [...TIPO_PAGO_CATALOGOS_ODS];
-    if (this.registroPago.valeP > 0 || this.registroPago.nss) {
-      tipoPago.shift()
+  validarAGF(idOds: number): Observable<HttpRespuesta<any>> {
+    return this.realizarPagoService.consultarIdODSAGF(idOds);
+  }
+
+  validarVale(idOds: number): Observable<HttpRespuesta<any>> {
+    return this.realizarPagoService.consultarIdODSVale(idOds);
+  }
+
+  filtrarCatalogosODS(): void {
+    const ID: number = this.registroPago.idRegistro;
+    this.cargadorService.activar();
+    forkJoin([this.validarAGF(ID), this.validarVale(ID)]).pipe(
+      finalize(() => this.cargadorService.desactivar())
+    ).subscribe({
+      next: (respuesta: [HttpRespuesta<any>, HttpRespuesta<any>]) => this.procesarRespuestaCatalogos(respuesta)
+    })
+  }
+
+  procesarRespuestaCatalogos(respuesta: [HttpRespuesta<any>, HttpRespuesta<any>]): void {
+    let CATALOGOS: TipoDropdown[] = [...TIPO_PAGO_CATALOGOS_ODS]
+    const POSICION_VALIDACION_AGF: number = 0;
+    const POSICION_VALIDACION_VALE: number = 1;
+    this.agfSeleccionado = respuesta[POSICION_VALIDACION_AGF].datos;
+    if (this.agfSeleccionado.agf === 0) {
+      CATALOGOS = CATALOGOS.filter((pago: TipoDropdown) => ![2].includes(pago.value as number));
     }
-    return tipoPago;
+    const valeSeleccionado = respuesta[POSICION_VALIDACION_VALE].datos
+    if (valeSeleccionado.valeP === 1) {
+      CATALOGOS = CATALOGOS.filter((pago: TipoDropdown) => ![1].includes(pago.value as number));
+    }
+    this.tiposPago = CATALOGOS;
   }
 
   seleccionarPago(): void {
@@ -221,6 +261,5 @@ export class ControlPagoComponent implements OnInit {
       importePago: this.registroPago.totalPorCubrir,
     }
   }
-
 
 }
