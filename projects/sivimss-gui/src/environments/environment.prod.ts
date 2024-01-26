a077265a771d2909969501a1cc917be9b79ee0f4
Sebastián Gómez <sebastian.gomez@people-media.com.mx>
2024-01-25 18:03:47 -0600

    Se agregan environment

--- projects/sivimss-gui/src/environments/environment.prod.ts
+++ projects/sivimss-gui/src/environments/environment.prod.ts
@@ -6,6 +6,7 @@ export const environment = {
     login: 'mssivimss-oauth/v1',
     servicios_externos: 'mssivimss-ser-externos/v1/catalogos/externos/',
     servicios_externos_reportes: 'mssivimss-ser-externos/v1/reportes/',
-    notificaciones: 'mssivimss-notificaciones/v1/notificaciones/avisos'
+    notificaciones: 'mssivimss-notificaciones/v1/notificaciones/avisos',
+    conveniosPF: 'mssivimss-ods-prefune-on/v1/',
   }
 };
--- projects/sivimss-gui/src/environments/environment.ts
+++ projects/sivimss-gui/src/environments/environment.ts
@@ -11,6 +11,7 @@ export const environment = {
     servicios_externos: 'mssivimss-ser-externos/v1/catalogos/externos/',
     servicios_externos_reportes: 'mssivimss-ser-externos/v1/reportes/',
     notificaciones: 'mssivimss-notificaciones/v1/notificaciones/avisos',
+    conveniosPF: 'mssivimss-ods-prefune-on/v1/',
 
     // mssivimss: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-ctrol-permisos/v1/sivimss/service/',
     // login: 'https://sivimss-ds.apps.ocp.imss.gob.mx/mssivimss-oauth/v1',
