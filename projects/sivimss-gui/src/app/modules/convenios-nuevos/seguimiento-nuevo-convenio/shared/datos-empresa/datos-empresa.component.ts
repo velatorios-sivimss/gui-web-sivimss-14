728516f364fd41e225ebc6e39a5c6c663ef528c1
epina-gdc <eduardo.gomez@people-media.com.mx>
2024-02-01 17:42:02 -0600

    validaciones datos empresa

--- projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/shared/datos-empresa/datos-empresa.component.ts
+++ projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/shared/datos-empresa/datos-empresa.component.ts
@@ -1,12 +1,16 @@
 import {Component, inject, OnInit} from '@angular/core';
-import {ControlContainer, ReactiveFormsModule} from "@angular/forms";
+import {ControlContainer, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {DropdownModule} from "primeng/dropdown";
+import {UtileriaModule} from "../../../../../shared/utileria/utileria.module";
+import {CommonModule} from "@angular/common";
+import {CalendarModule} from "primeng/calendar";
 
 @Component({
   selector: 'app-datos-empresa',
   templateUrl: './datos-empresa.component.html',
   styleUrls: ['./datos-empresa.component.scss'],
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, DropdownModule, UtileriaModule, CommonModule, CalendarModule],
   viewProviders: [
     {
       provide: ControlContainer,
@@ -16,9 +20,16 @@ import {ControlContainer, ReactiveFormsModule} from "@angular/forms";
 })
 export class DatosEmpresaComponent implements OnInit {
 
-  constructor() { }
+  parentContainer: ControlContainer = inject(ControlContainer)
+
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  get parentFormGroup() {
+    return (this.parentContainer.control as FormGroup).controls
   }
 
 }
