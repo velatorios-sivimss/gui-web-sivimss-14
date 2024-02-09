28820ca137b5a82c45afaef1e6be1ad6b5cbef86
GRUPOPM\mparedes <martin.paredes@people-media.com.mx>
2024-02-09 17:58:43 -0600

    Se elimina codigo comentado

--- projects/sivimss-gui/src/app/pages/publico/pages/registro/registro.module.ts
+++ projects/sivimss-gui/src/app/pages/publico/pages/registro/registro.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RegistroRoutingModule } from './registro-routing.module';
 import { RegistroComponent } from './registro.component';
 import { CalendarModule } from 'primeng/calendar';
 import { DropdownModule } from 'primeng/dropdown';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TituloPrincipalModule } from 'projects/sivimss-gui/src/app/shared/titulo-principal/titulo-principal.module';
-import { ContratantesService } from 'projects/sivimss-gui/src/app/modules/contratantes/services/contratantes.service';
 import { UtileriaModule } from 'projects/sivimss-gui/src/app/shared/utileria/utileria.module';
 import { UsuarioService } from 'projects/sivimss-gui/src/app/modules/usuarios/services/usuario.service';
 import { RegistroService } from './services/registro.service';
