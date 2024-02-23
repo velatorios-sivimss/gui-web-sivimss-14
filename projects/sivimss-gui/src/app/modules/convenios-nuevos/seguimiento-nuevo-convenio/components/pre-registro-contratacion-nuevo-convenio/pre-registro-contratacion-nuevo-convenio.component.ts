00a67c2cee4b1e1262351ca4907883178b7bfa13
epina-gdc <eduardo.gomez@people-media.com.mx>
2024-02-23 10:48:25 -0600

    corrección envio informacion doc persona

--- projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/components/pre-registro-contratacion-nuevo-convenio/pre-registro-contratacion-nuevo-convenio.component.ts
+++ projects/sivimss-gui/src/app/modules/convenios-nuevos/seguimiento-nuevo-convenio/components/pre-registro-contratacion-nuevo-convenio/pre-registro-contratacion-nuevo-convenio.component.ts
@@ -465,7 +465,7 @@ export class PreRegistroContratacionNuevoConvenioComponent {
       enfermedadPreExistente: [{value: +solicitante.enfermedadPre, disabled: true}],
       tipoPaquete: [{value: solicitante.idPaquete, disabled: false}, [Validators.required]],
       fechaNaciemiento: [{value: solicitante.fechaNaciemiento, disabled: true}],
-      idContraPaqPF: [{value: solicitante.idContraPaqPF, disabled: true}],
+      idContraPaqPF: [{value: solicitante.idPaqueteConvenio, disabled: true}],
       idContrantante: [{value: solicitante.idContratante, disabled: true}],
       idConvenioPF: [{value: this.idConvenio, disabled: true}],
       idDomicilio: [{value: solicitante.idDomicilio, disabled: true}],
