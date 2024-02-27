3f33223c6ae4fd4c328916a11ef214a7a3050b9e
epina-gdc <eduardo.gomez@people-media.com.mx>
2024-02-27 16:38:12 -0600

    prueba variable global

--- projects/sivimss-gui/src/app/modules/inicio/components/inicio/inicio.component.ts
+++ projects/sivimss-gui/src/app/modules/inicio/components/inicio/inicio.component.ts
@@ -19,8 +19,8 @@ export class InicioComponent implements OnInit {
 
   ngOnInit(): void {
     this.breadcrumbService.limpiar();
-    console.log(environment.api.token)
-    console.log(environment.api.notificaciones)
+    console.log(`${environment.api.token}`)
+    console.log(`${environment.api.mssivimss}`)
     // console.log(window)
     this.anuncio = [
       {image: "slide1.jpg"},
