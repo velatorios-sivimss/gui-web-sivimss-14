aafb16cc476eb2b01f09187f1a60e142633bf296
epina-gdc <eduardo.gomez@people-media.com.mx>
2024-03-06 11:49:26 -0600

    corrección directiva only numbers

--- projects/sivimss-gui/src/app/shared/utileria/directives/only-numbers.directive.ts
+++ projects/sivimss-gui/src/app/shared/utileria/directives/only-numbers.directive.ts
@@ -1,21 +1,62 @@
-import { Directive, ElementRef, HostListener, Output, EventEmitter } from '@angular/core';
+import {Directive, ElementRef, HostListener, forwardRef, Renderer2} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Directive({
-  selector: 'input[numbersOnly]'
+  selector: 'input[numbersOnly]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => NumberDirective),
+    multi: true
+  }]
 })
-export class NumberDirective {
+export class NumberDirective implements ControlValueAccessor {
+  private onChange!: (val: string) => void;
+  private onTouched!: () => void;
+  private value!: string;
 
-@Output() valueChange = new EventEmitter()
-  constructor(private _el: ElementRef) { }
-
-  @HostListener('input', ['$event']) onInputChange(event: any) {
-    const initalValue = this._el.nativeElement.value;
-    const newValue = initalValue.replace(/\D*/g, '');
-       this._el.nativeElement.value = newValue;
-       this.valueChange.emit(newValue);
-    if ( initalValue !== this._el.nativeElement.value) {
-      event.stopPropagation();
-    }
+  constructor(
+    private elementRef: ElementRef,
+    private renderer: Renderer2
+  ) {
   }
 
+  @HostListener('input', ['$event.target.value'])
+  onInputChange(value: string): void {
+    const valorFiltrado: string = filtrarValor(value);
+    this.actualizarTextInput(valorFiltrado, this.value !== valorFiltrado);
+  }
+
+  @HostListener('blur')
+  onBlur(): void {
+    this.onTouched();
+  }
+
+  private actualizarTextInput(nuevoValor: string, propagarCambio: boolean): void {
+    this.renderer.setProperty(this.elementRef.nativeElement, 'value', nuevoValor);
+    if (propagarCambio) {
+      this.onChange(nuevoValor);
+    }
+    this.value = nuevoValor;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.renderer.setProperty(this.elementRef.nativeElement, 'disabled', isDisabled);
+  }
+
+  writeValue(value: any): void {
+    value = value ? String(value) : '';
+    this.actualizarTextInput(value, false);
+  }
+}
+
+function filtrarValor(value: string): string {
+  return value.replace(/\D*/g, '');
 }
